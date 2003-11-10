f58bdc1050fda0629fd3053a99ec29a170e50feb
rkulp <rkulp@f56db7ec-86c8-4192-a518-b33116ffafde>
2003-11-10 21:35:19 +0000

    [46375] Customizer action deadlocked on invalidate proxy

--- source/org.eclipse.ve.java.core/core/org/eclipse/ve/internal/java/core/CustomizeAction.java
+++ source/org.eclipse.ve.java.core/core/org/eclipse/ve/internal/java/core/CustomizeAction.java
@@ -11,7 +11,7 @@ package org.eclipse.ve.internal.java.core;
  *******************************************************************************/
 /*
  *  $RCSfile: CustomizeAction.java,v $
- *  $Revision: 1.1 $  $Date: 2003-10-27 17:48:30 $ 
+ *  $Revision: 1.2 $  $Date: 2003-11-10 21:35:19 $ 
  */
 
 import java.util.List;
@@ -19,18 +19,17 @@ import java.util.List;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.ui.actions.SelectionAction;
+import org.eclipse.jem.internal.beaninfo.BeanDecorator;
+import org.eclipse.jem.internal.beaninfo.BeaninfoPackage;
+import org.eclipse.jem.internal.core.MsgLogger;
+import org.eclipse.jem.internal.instantiation.base.*;
+import org.eclipse.jem.internal.java.JavaClass;
+import org.eclipse.jem.internal.proxy.core.*;
 import org.eclipse.ui.IWorkbenchPart;
 
-import org.eclipse.jem.internal.beaninfo.BeanDecorator;
-import org.eclipse.jem.internal.beaninfo.adapters.Utilities;
-import org.eclipse.jem.internal.core.*;
-import org.eclipse.jem.internal.instantiation.base.*;
-import org.eclipse.jem.internal.proxy.core.*;
-
+import org.eclipse.ve.internal.cde.emf.ClassDecoratorFeatureAccess;
 import org.eclipse.ve.internal.java.common.Common;
 import org.eclipse.ve.internal.propertysheet.common.commands.CompoundCommand;
-
-import org.eclipse.jem.internal.java.JavaClass;
 
 public class CustomizeAction extends SelectionAction {
 	public static final String ACTION_ID = "jcm.CUSTOMIZE"; //$NON-NLS-1$
@@ -65,7 +64,7 @@ public class CustomizeAction extends SelectionAction {
 			if (ba instanceof BeanProxyAdapter && ((BeanProxyAdapter) ba).isThisPart())
 				return null;
 			JavaClass beanClass = (JavaClass) ((EObject) model).eClass();
-			BeanDecorator beanDecor = Utilities.getBeanDecorator(beanClass);
+			BeanDecorator beanDecor = (BeanDecorator) ClassDecoratorFeatureAccess.getDecoratorWithFeature(beanClass, BeanDecorator.class, BeaninfoPackage.eINSTANCE.getBeanDecorator_CustomizerClass());
 			return beanDecor != null ? beanDecor.getCustomizerClass() : null;	// If class invalid, bean decor will be null.
 		}
 		return null;
