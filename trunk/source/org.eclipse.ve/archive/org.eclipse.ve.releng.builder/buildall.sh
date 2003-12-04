f7dfa3d9ead7ab041a31987a7acb422c2d7498dc
rkulp <rkulp@f56db7ec-86c8-4192-a518-b33116ffafde>
2003-12-04 22:53:01 +0000

    Sync up the ve100 build to the ve050 base for auto builds on Linux.

---
+++ trunk/source/org.eclipse.ve/archive/org.eclipse.ve.releng.builder/buildall.sh
@@ -0,0 +1,73 @@
+# !/bin/sh
+
+usage() {
+	echo "usage: buildAll [-mapVersionTag HEAD|<branch name>] [-vm <url to java executable to run build>] [-bc <bootclasspath>] [-target <buildall target to execute>] [-buildID <buildID, e.g. 2.1.2>]  [-ftp <userid> <password>] I|M"
+}
+
+# tag to use when checking out .map file project
+mapVersionTag=HEAD
+
+# default setting for buildType
+buildType=""
+
+# default setting for buildID
+buildID=""
+
+# default bootclasspath
+bootclasspath=""
+
+# vm used to run the build.  Defaults to java on system path
+vm=java
+
+# target used if not default (to allow run just a portion of buildAll)
+target=""
+
+# FTP user/password, required for Windows to ftp. Without it, no push.
+ftpUser=""
+ftpPassword=""
+
+if [ "x$1" == "x" ] ; then
+        usage
+        exit 0
+fi
+
+while [ "$#" -gt 0 ] ; do
+        case $1 in
+                '-mapVersionTag')
+                        mapVersionTag=$2;
+                        shift 1
+                    ;;
+                '-vm')
+                        vm=$2;
+                        shift 1
+                    ;;
+                '-bc')
+                        bootclasspath="-Dbootclasspath=$2";
+                        shift 1
+                    ;;
+                '-target')
+                        target=$2;
+                        shift 1
+                    ;;
+                '-buildID')
+                        buildID="-DbuildId=$2";
+                        shift 1
+                    ;;
+                '-ftp')
+                        ftpUser="-DftpUser=$2";
+                        ftpPassword="-DftpPassword=$3"
+                        shift 2
+                    ;;
+                *)
+                        buildType=$1
+                    ;;
+        esac
+        shift 1
+done
+
+if [ -z $buildType ] ; then
+	usage
+	exit 0
+fi
+
+$vm -cp ../org.eclipse.releng.basebuilder/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -f buildAll.xml $target $bootclasspath -Dplatform=LinuxGTK -DmapVersionTag=$mapVersionTag -DbuildType=$buildType $buildID $ftpUser $ftpPassword
