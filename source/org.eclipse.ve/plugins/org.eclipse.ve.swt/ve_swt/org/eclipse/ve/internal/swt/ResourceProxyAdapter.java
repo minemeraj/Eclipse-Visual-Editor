b84658a70baff62b675b3f239606dab700d63597
winchest <winchest@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-01-22 20:15:17 +0000

    Fix to get the target VM instantiation to occur on the Display thread

--- source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ResourceProxyAdapter.java
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ResourceProxyAdapter.java
@@ -1,13 +1,7 @@
 package org.eclipse.ve.internal.swt;
 
-import java.text.MessageFormat;
-
-import org.eclipse.emf.common.notify.Adapter;
-import org.eclipse.emf.ecore.EObject;
-
 import org.eclipse.ve.internal.java.core.*;
 
-import org.eclipse.jem.internal.core.MsgLogger;
 import org.eclipse.jem.internal.instantiation.InitStringAllocation;
 import org.eclipse.jem.internal.instantiation.JavaAllocation;
 import org.eclipse.jem.internal.instantiation.base.IJavaInstance;
@@ -18,40 +12,29 @@ public class ResourceProxyAdapter extends BeanProxyAdapter {
 public ResourceProxyAdapter(IBeanProxyDomain domain) {
 	super(domain);
 }
-protected Adapter getRegisteredAdapter(EObject eo, Object adapterType) {
-	
-	Adapter result = super.getRegisteredAdapter(eo,adapterType);
-	if(result instanceof InitStringAllocationAdapter){
-		eo.eAdapters().remove(result);
-		result = new InitStringAllocationAdapter(){
-			public IBeanProxy allocate(JavaAllocation allocation, IBeanProxyDomain domain) throws AllocationException {
-				InitStringAllocation initStringAllocation = (InitStringAllocation) allocation;
-				// The container of the allocation is the IJavaInstance being instantiated.
-				String qualifiedClassName = ((IJavaInstance) allocation.eContainer()).getJavaType().getQualifiedNameForReflection();
-				String initString = initStringAllocation.getAllocString();
-				IBeanTypeProxy targetClass = domain.getProxyFactoryRegistry().getBeanTypeProxyFactory().getBeanTypeProxy(qualifiedClassName);
-				if (targetClass == null || targetClass.getInitializationError() != null) {
-					// The target class is invalid.
-					Throwable exc = new ExceptionInInitializerError(targetClass != null ? targetClass.getInitializationError() : MessageFormat.format(JavaMessages.getString("Proxy_Class_has_Errors_ERROR_"), new Object[] {"unknown"})); //$NON-NLS-1$
-					JavaVEPlugin.log("Could not instantiate " + (targetClass != null ? targetClass.getTypeName() : "unknown") + " with initialization string=" + initString, MsgLogger.LOG_WARNING); //$NON-NLS-1$ //$NON-NLS-2$
-					JavaVEPlugin.log(exc, MsgLogger.LOG_WARNING);
-					throw new AllocationException(exc);			
-				}
-				try { 					
-					IBeanTypeProxy environmentBeanTypeProxy = domain.getProxyFactoryRegistry().getBeanTypeProxyFactory().getBeanTypeProxy("com.ibm.etools.jbcf.swt.targetvm.Environment"); //$NON-NLS-1$
-					IMethodProxy evaluateInitStringMethod = environmentBeanTypeProxy.getMethodProxy("evaluate","java.lang.String");
-					IBeanProxy initStringProxy = domain.getProxyFactoryRegistry().getBeanProxyFactory().createBeanProxyWith(initString);
-					return evaluateInitStringMethod.invoke(environmentBeanTypeProxy,initStringProxy);
-				} catch (Exception exc) {
-				}
-				return null;
-			}
-		};
-		result.setTarget(eo);
-		eo.eAdapters().add(result);
-		return result;
+/**
+ * The initString is evaluated using a static method on the Environment target VM class
+ * that ensures it is evaluated on the Display thread
+ */
+protected void basicInitializationStringAllocation(String aString, IBeanTypeProxy targetClass){
+	// TODO Get rid of this hack as soon as we can
+	IBeanTypeProxy environmentBeanTypeProxy = domain.getProxyFactoryRegistry().getBeanTypeProxyFactory().getBeanTypeProxy("com.ibm.etools.jbcf.swt.targetvm.Environment"); //$NON-NLS-1$
+	IMethodProxy evaluateInitStringMethod = environmentBeanTypeProxy.getMethodProxy("evaluate","java.lang.String");
+	IBeanProxy initStringProxy = domain.getProxyFactoryRegistry().getBeanProxyFactory().createBeanProxyWith(aString);
+	try {
+		setupBeanProxy(evaluateInitStringMethod.invoke(environmentBeanTypeProxy,initStringProxy));
+	} catch (ThrowableProxy e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-	return result;
 }	
+protected void beanProxyAllocation(IBeanProxyDomain beanProxyDomain, JavaAllocation allocation)  {
+	
+	if(allocation instanceof InitStringAllocation){
+		InitStringAllocation initStringAllocation = (InitStringAllocation)allocation;
+		String qualifiedClassName = ((IJavaInstance) initStringAllocation.eContainer()).getJavaType().getQualifiedNameForReflection();
+		basicInitializationStringAllocation(initStringAllocation.getInitString(), beanProxyDomain.getProxyFactoryRegistry().getBeanTypeProxyFactory().getBeanTypeProxy(qualifiedClassName));
+	}
+}
 
 }
