f5ac03b40a8f447d7638f2b075481c3103184f23
gmendel <gmendel@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-01-23 12:45:11 +0000

    Create a codegen package

---
+++ source/org.eclipse.ve.swt/swtcodegen/org/eclipse/ve/internal/swt/codegen/CompositeDecoder.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*
+ *  $RCSfile: CompositeDecoder.java,v $
+ *  $Revision: 1.1 $  $Date: 2004-01-23 12:45:11 $ 
+ */
+package org.eclipse.ve.internal.swt.codegen;
+
+import org.eclipse.ve.internal.java.codegen.core.IDiagramModelInstance;
+import org.eclipse.ve.internal.java.codegen.model.*;
+ 
+/**
+ * @author Gili Mendel
+ * @since 1.0.0
+ */
+public class CompositeDecoder extends AbstractCompositeDecoder {
+
+	/**
+	 * @param expr
+	 * @param model
+	 * @param cm
+	 * @param part
+	 * 
+	 * @since 1.0.0
+	 */
+	public CompositeDecoder(CodeExpressionRef expr, IBeanDeclModel model, IDiagramModelInstance cm, BeanPart part) {
+		super(expr, model, cm, part);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 * 
+	 * @since 1.0.0
+	 */
+	public CompositeDecoder() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
