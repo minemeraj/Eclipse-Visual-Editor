7717599f0c2937bbfb17b804a94543c799ad847d
rkulp <rkulp@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-02-12 19:55:11 +0000

    Fix container to handle generic platform swt on M7. Previous was hard-coded and didn't work with M7.

--- trunk/source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/SWTContainer.java
+++ trunk/source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/SWTContainer.java
@@ -1,5 +1,6 @@
 package org.eclipse.ve.internal.swt;
 
+import java.io.IOException;
 import java.net.URL;
 
 import org.eclipse.core.runtime.IPath;
@@ -7,6 +8,10 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.*;
 import org.eclipse.jdt.internal.core.*;
+
+import org.eclipse.jem.internal.core.MsgLogger;
+
+import org.eclipse.ve.internal.java.core.JavaVEPlugin;
 
 
 public class SWTContainer implements IClasspathContainer {
@@ -16,19 +21,24 @@ public class SWTContainer implements IClasspathContainer {
 	
 	public SWTContainer(){
 		// This is a container that resolves to the path of whever SWT is installed in the workbench
-		Path path = new Path("ws/win32/swt.jar");
+		Path path = new Path("$ws$/swt.jar");
 		URL location = Platform.getPlugin("org.eclipse.swt").find(path);
-		fPath = new Path(location.getFile());
-		fClasspathEntries = new IClasspathEntry[] {
-			new ClasspathEntry(IPackageFragmentRoot.K_BINARY,
-				IClasspathEntry.CPE_LIBRARY,
-				JavaProject.canonicalizedPath(fPath),
-				ClasspathEntry.NO_EXCLUSION_PATTERNS,
-				null,
-				null,
-				null,
-				true)
-		};
+		try {
+			fPath = new Path(Platform.resolve(location).getFile());
+			fClasspathEntries =
+				new IClasspathEntry[] {
+					 new ClasspathEntry(
+						IPackageFragmentRoot.K_BINARY,
+						IClasspathEntry.CPE_LIBRARY,
+						JavaProject.canonicalizedPath(fPath),
+						ClasspathEntry.NO_EXCLUSION_PATTERNS,
+						null,
+						null,
+						null,
+						true)};
+		} catch (IOException e) {
+			JavaVEPlugin.log(e, MsgLogger.LOG_INFO);
+		}
 	}
 
 	public IClasspathEntry[] getClasspathEntries() {
