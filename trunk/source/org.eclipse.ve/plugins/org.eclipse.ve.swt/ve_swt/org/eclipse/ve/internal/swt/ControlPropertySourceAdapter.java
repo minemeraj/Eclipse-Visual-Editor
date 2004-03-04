046bf5d7911e3622f592d6f619757fcf6383ae55
winchest <winchest@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-03-04 19:29:45 +0000

    LayoutData works for RowData where there is no actual value

--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ControlPropertySourceAdapter.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ControlPropertySourceAdapter.java
@@ -10,18 +10,23 @@ package org.eclipse.ve.internal.swt;
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /*
- *  $RCSfile: CompositePropertySourceAdapter.java,v $
- *  $Revision: 1.1 $  $Date: 2004-03-04 17:00:21 $ 
+ *  $RCSfile: ControlPropertySourceAdapter.java,v $
+ *  $Revision: 1.1 $  $Date: 2004-03-04 19:28:57 $ 
  */
 import java.util.*;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
+import org.eclipse.ve.internal.cde.core.EditDomain;
 import org.eclipse.ve.internal.cde.emf.InverseMaintenanceAdapter;
 import org.eclipse.jem.internal.beaninfo.adapters.Utilities;
 import org.eclipse.jem.internal.instantiation.base.IJavaObjectInstance;
 import org.eclipse.jem.internal.instantiation.base.JavaInstantiation;
 import org.eclipse.ve.internal.java.core.*;
 import org.eclipse.ve.internal.java.core.BeanProxyUtilities;
+import org.eclipse.ve.internal.java.visual.ILayoutPolicyFactory;
+import org.eclipse.ve.internal.java.visual.VisualUtilities;
+
 import org.eclipse.jem.internal.proxy.core.IBeanProxy;
 import org.eclipse.jem.java.JavaClass;
 /**
@@ -31,17 +36,20 @@ import org.eclipse.jem.java.JavaClass;
  * otherwise they are read only
  */
 
-public class CompositePropertySourceAdapter extends BeanPropertySourceAdapter {
+public class ControlPropertySourceAdapter extends BeanPropertySourceAdapter {
 	
 	public IPropertyDescriptor[] getPropertyDescriptors() {
 		
 		IPropertyDescriptor[] descriptors = super.getPropertyDescriptors();
-		
-		IJavaObjectInstance compositeJavaObjectInstance = (IJavaObjectInstance) InverseMaintenanceAdapter.getFirstReferencedBy(
-				getEObject(),
-				JavaInstantiation.getReference(
-						(JavaClass) Utilities.getJavaClass("org.eclipse.swt.widgets.Composite",getEObject().eResource().getResourceSet()),
-						SWTConstants.SF_COMPOSITE_CONTROLS));
+
+		IJavaObjectInstance compositeJavaObjectInstance = null;
+		if(getEObject().eResource() != null) {
+			compositeJavaObjectInstance = (IJavaObjectInstance) InverseMaintenanceAdapter.getFirstReferencedBy(
+					getEObject(),
+					JavaInstantiation.getReference(
+							(JavaClass) Utilities.getJavaClass("org.eclipse.swt.widgets.Composite",getEObject().eResource().getResourceSet()),
+							SWTConstants.SF_COMPOSITE_CONTROLS));
+		}
 		
 		boolean explicitUserSizing = false;
 		// Top level things like Shells don't have a parent
@@ -57,7 +65,8 @@ public class CompositePropertySourceAdapter extends BeanPropertySourceAdapter {
 		loop: for (int i = 0; i<descriptors.length; i++) {
 			IPropertyDescriptor pd = descriptors[i];
 			if (pd.getId() instanceof EStructuralFeature) {
-				String fn = ((EStructuralFeature) pd.getId()).getName();				
+				EStructuralFeature sf = (EStructuralFeature)pd.getId();
+				String fn = sf.getName();				
 				if(explicitUserSizing) {
 					// exclude the layoutData property
 					if ("layoutData".equals(fn))
@@ -67,7 +76,25 @@ public class CompositePropertySourceAdapter extends BeanPropertySourceAdapter {
 					if ("bounds".equals(fn) || "size".equals(fn) || "location".equals(fn)) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 						continue loop;
 				}
-				descriptorList.add(pd);
+				// LayoutData is wrappered so it is treated differently to allow values on un-set layoutData instances to be set
+				if("layoutData".equals(fn)){
+					// We need the class of the layoutData to set.  This comes from looking at the policyFactory
+					// for our container's layoutManager
+					CompositeProxyAdapter compositeProxyAdapter = (CompositeProxyAdapter)BeanProxyUtilities.getBeanProxyHost(compositeJavaObjectInstance);					
+					EditDomain domain = compositeProxyAdapter.getBeanProxyDomain().getEditDomain();
+					ILayoutPolicyFactory factory = VisualUtilities.getLayoutPolicyFactory(compositeProxyAdapter.getLayoutBeanProxy().getTypeProxy(),domain);
+					LayoutDataPropertyDescriptor layoutPD = null;
+					if(factory != null){
+						layoutPD = (LayoutDataPropertyDescriptor) factory.getConstraintPropertyDescriptor(sf);
+					} else {
+						layoutPD = new LayoutDataPropertyDescriptor(null);
+					}
+					layoutPD.setPropertyDescriptor(pd);
+					layoutPD.setEditDomain(domain);
+					descriptorList.add(layoutPD);
+				} else {
+					descriptorList.add(pd);
+				}
 			}
 		}
 		return (IPropertyDescriptor[]) descriptorList.toArray(new IPropertyDescriptor[descriptorList.size()]);
