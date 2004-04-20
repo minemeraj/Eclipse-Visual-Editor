a1f9d65b5f14585d3057c4c10a36934fa07c3efc
winchest <winchest@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-04-20 09:13:12 +0000

    JTable enhancements

---
+++ trunk/source/org.eclipse.ve.jfc/jfc/org/eclipse/ve/internal/jfc/core/TableColumnGraphicalEditPart.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*
+ *  $RCSfile: TableColumnGraphicalEditPart.java,v $
+ *  $Revision: 1.1 $  $Date: 2004-04-20 09:13:12 $ 
+ */
+package org.eclipse.ve.internal.jfc.core;
+
+import org.eclipse.draw2d.*;
+import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gef.GraphicalEditPart;
+import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
+
+import org.eclipse.ve.internal.cde.core.OutlineBorder;
+ 
+
+/**
+ * javax.swing.table.TableColumn does not inherit from java.awt.Component
+ * The edit part should create a figure that has the height of the parent JTable
+ * the width of getWidth() from the TableColumn
+ * and the x based on the width of all the preceeding columns
+ * @since 1.0.0
+ */
+public class TableColumnGraphicalEditPart extends AbstractGraphicalEditPart {
+
+	protected IFigure createFigure() {
+		Figure figure = new Figure();
+		figure.setOpaque(true);
+		figure.setBackgroundColor(ColorConstants.cyan);
+		figure.setBorder(new OutlineBorder());
+		figure.setSize(30,30);
+		Rectangle parentBounds = ((GraphicalEditPart)getParent()).getFigure().getBounds();
+		figure.setLocation(new Point(parentBounds.x,parentBounds.y));
+		figure.setSize(parentBounds.width/2,parentBounds.height);
+		return figure;
+	}
+	
+	
+	public void refresh(){
+		super.refresh();
+		Rectangle parentBounds = ((GraphicalEditPart)getParent()).getFigure().getBounds();
+		getFigure().setLocation(new Point(parentBounds.x,parentBounds.y));
+		getFigure().setSize(parentBounds.width/2,parentBounds.height);
+	}
+
+	protected void createEditPolicies() {
+		// TODO Auto-generated method stub
+	}
+}
