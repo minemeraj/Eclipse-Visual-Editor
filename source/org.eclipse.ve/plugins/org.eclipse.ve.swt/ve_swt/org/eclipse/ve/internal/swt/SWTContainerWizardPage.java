45a790eccdc1683c66d96f7f9918de5dc7699b8e
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-05-18 14:44:12 +0000

    Commenting out useless configuration options, spiffing up wizard page look.

--- source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/SWTContainerWizardPage.java
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/SWTContainerWizardPage.java
@@ -3,6 +3,7 @@ package org.eclipse.ve.internal.swt;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.ui.JavaPluginImages;
 import org.eclipse.jdt.ui.wizards.IClasspathContainerPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -16,7 +17,10 @@ public class SWTContainerWizardPage extends WizardPage implements IClasspathCont
 	private Button includeJFaceButton = null;
 	
 	public SWTContainerWizardPage() {
-		super("Standard Widget Toolkit (SWT)");
+		super("Standard Widget Toolkit (SWT)"); //$NON-NLS-1$
+		setTitle(SWTMessages.getString("SWTContainerWizardPage.wizardTitle")); //$NON-NLS-1$
+		setMessage(SWTMessages.getString("SWTContainerWizardPage.wizardMessage")); //$NON-NLS-1$
+		setImageDescriptor(JavaPluginImages.DESC_WIZBAN_ADD_LIBRARY);
 	}	
 	
 	public void createControl(Composite parent) {
@@ -24,17 +28,17 @@ public class SWTContainerWizardPage extends WizardPage implements IClasspathCont
 		Composite c = new Composite(parent,SWT.NONE);
 		c.setLayout(new GridLayout());
 		// First and default option is to use the SWT level with Eclipse itself
-		Button check = new Button(c,SWT.RADIO);
+/*		Button check = new Button(c,SWT.RADIO);
 		check.setText("Use platform SWT level");
 		check.setSelection(true);
 //		check.setEnabled(false); 
 		// Second option is to use the SWT level with the project in the build path
 		Button useBuildLocation = new Button(c,SWT.RADIO);
 		useBuildLocation.setText("Use \"org.eclipse.swt\" project in the build path");
-		useBuildLocation.setEnabled(false);
+		useBuildLocation.setEnabled(false);*/
 		
 		includeJFaceButton = new Button(c, SWT.CHECK);
-		includeJFaceButton.setText("Include support for JFace library");
+		includeJFaceButton.setText(SWTMessages.getString("SWTContainerWizardPage.includeJFaceCheck")); //$NON-NLS-1$
 		
 		
 		setControl(c);
@@ -44,9 +48,9 @@ public class SWTContainerWizardPage extends WizardPage implements IClasspathCont
 	public boolean finish(){
 		Path path;
 		if (includeJFaceButton != null && includeJFaceButton.getSelection()) {
-			path = new Path("SWT_CONTAINER/JFACE");
+			path = new Path("SWT_CONTAINER/JFACE"); //$NON-NLS-1$
 		} else {
-			path = new Path("SWT_CONTAINER");
+			path = new Path("SWT_CONTAINER"); //$NON-NLS-1$
 		}
 		setSelection(JavaCore.newContainerEntry(path));			
 		return true;
