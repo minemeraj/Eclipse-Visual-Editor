5fa5ae76523d3c9a54e1bddd27b972c708f09e55
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-05-26 19:56:06 +0000

    Adding IJavaBeanContextMenuContributor back in for quick filtering of Java Beans

---
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.java.core/core/org/eclipse/ve/internal/java/core/IJavaBeanContextMenuContributor.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*
+ *  $RCSfile: IJavaBeanContextMenuContributor.java,v $
+ *  $Revision: 1.3 $  $Date: 2004-05-26 19:56:06 $ 
+ */
+package org.eclipse.ve.internal.java.core;
+
+import org.eclipse.ve.internal.cde.core.ICDEContextMenuContributor;
+ 
+
+/**
+ * This interface allows us to determine quickly which EditParts are Java Beans.
+ */
+public interface IJavaBeanContextMenuContributor extends ICDEContextMenuContributor {
+
+}
