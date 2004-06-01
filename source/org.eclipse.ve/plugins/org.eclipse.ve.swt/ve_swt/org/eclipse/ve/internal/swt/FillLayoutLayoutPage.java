8165cbcf6accbfe0c656d5bb157add027b9d9e71
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-06-01 21:07:55 +0000

    Add customize layout to context menu, and make layout customization context sensitive

--- source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/FillLayoutLayoutPage.java
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/FillLayoutLayoutPage.java
@@ -10,7 +10,7 @@
  *******************************************************************************/
 /*
  *  $RCSfile: FillLayoutLayoutPage.java,v $
- *  $Revision: 1.2 $  $Date: 2004-05-26 18:23:33 $ 
+ *  $Revision: 1.3 $  $Date: 2004-06-01 21:07:54 $ 
  */
 package org.eclipse.ve.internal.swt;
 
@@ -231,6 +231,24 @@ public class FillLayoutLayoutPage extends CustomizeLayoutPage {
 		IActionFilter af = (IActionFilter) ((IAdaptable) target).getAdapter(IActionFilter.class);
 		if (af != null && af.testAttribute(target, LAYOUT_FILTER_KEY, FillLayoutEditPolicy.LAYOUT_ID)) { //$NON-NLS-1$ //$NON-NLS-2$
 			return true;
+		}
+		return false;
+	}
+	
+	protected boolean selectionIsContainer(ISelection oldSelection) {
+		ISelection newSelection = getSelection();
+		if (newSelection != null && newSelection instanceof IStructuredSelection && !((IStructuredSelection) newSelection).isEmpty()) {
+			List editparts = ((IStructuredSelection) newSelection).toList();
+			EditPart firstParent;
+			
+			// Check to see if this is a single selected container
+			if (editparts.size() == 1 && editparts.get(0) instanceof EditPart) {
+				firstParent = (EditPart)editparts.get(0);
+				// check to see if this is a container with a GridLayout
+				if (isValidTarget(firstParent)) {
+					return true;
+				}
+			}
 		}
 		return false;
 	}
