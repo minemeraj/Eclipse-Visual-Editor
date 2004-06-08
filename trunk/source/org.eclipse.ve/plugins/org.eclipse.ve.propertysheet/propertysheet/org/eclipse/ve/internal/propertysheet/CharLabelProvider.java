b735a3d515ce3ee255895d2774ec46622f773e3a
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-06-08 19:42:35 +0000

    58274 - added character label provider support for displaying characters in the PS

---
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.propertysheet/propertysheet/org/eclipse/ve/internal/propertysheet/CharLabelProvider.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2001, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ve.internal.propertysheet;
+
+import org.eclipse.jface.viewers.LabelProvider;
+
+/**
+ * Label provider for Character/char values.
+ */
+public class CharLabelProvider extends LabelProvider {
+
+    /**
+     * Returns a Character as a string.
+     * @see org.eclipse.jface.viewers.ILabelProvider#getText(java.lang.Object)
+     */
+    public String getText(Object element) {
+        if(element instanceof Character) {
+            return ((Character) element).toString();
+        }
+        return super.getText(element);
+    }
+    
+}
