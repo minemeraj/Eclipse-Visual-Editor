3dc0378e496ca0ca767f7333b72f2865bc9041c8
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-06-08 16:29:21 +0000

    65751 - show SWT color property correctly in PS using label provider

---
+++ trunk/source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ColorJavaClassLabelProvider.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2001, 2003 IBM Corporation and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Common Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ve.internal.swt;
+
+import org.eclipse.jface.viewers.LabelProvider;
+
+import org.eclipse.jem.internal.instantiation.base.IJavaInstance;
+
+import org.eclipse.ve.internal.java.core.BeanProxyUtilities;
+
+/**
+ * Provides label for Color elements in SWT Properties view. See Color.override
+ */
+public class ColorJavaClassLabelProvider extends LabelProvider {
+
+	public String getText(Object element) {
+		if (element instanceof IJavaInstance) {
+			return BeanProxyUtilities.getBeanProxy((IJavaInstance) element).toBeanString();
+		} else {
+			return "";
+		}
+	}
+
+}
