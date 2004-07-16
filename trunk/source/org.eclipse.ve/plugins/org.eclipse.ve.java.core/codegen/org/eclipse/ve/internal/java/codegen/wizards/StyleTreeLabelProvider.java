87b7e358bce2d87711514cbfd4b3ce6a593b82de
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-07-16 14:56:26 +0000

    64498 - uses icons from palette in style tree... also added additional Swing/AWT extensions

--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.java.core/codegen/org/eclipse/ve/internal/java/codegen/wizards/StyleTreeLabelProvider.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.java.core/codegen/org/eclipse/ve/internal/java/codegen/wizards/StyleTreeLabelProvider.java
@@ -13,11 +13,14 @@ package org.eclipse.ve.internal.java.codegen.wizards;
 
 import java.util.*;
 
-import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
+
+import org.eclipse.ve.internal.cde.core.CDEPlugin;
 
 /**
  * @author pmuldoon
@@ -26,48 +29,58 @@ import org.eclipse.ui.PlatformUI;
  */
 
 public class StyleTreeLabelProvider extends LabelProvider {
-    private Map imageCache = new HashMap(11);
 
-    public Image getImage(Object element) {
-        ImageDescriptor descriptor = null;
-        ISharedImages images = PlatformUI.getWorkbench().getSharedImages();
-        if (element instanceof CategoryModel) {
-            descriptor = images.getImageDescriptor("IMG_OBJ_FOLDER");
+	private Map imageCache = new HashMap(11);
 
-            Image image = (Image) imageCache.get(descriptor);
-            if (image == null) {
-                image = descriptor.createImage();
-                imageCache.put(descriptor, image);
-            }
-            return image;
-        }
-        return null;
-    }
-    
-    /*
-     * @see ILabelProvider#getText(Object)
-     */
-    public String getText(Object element) {
-        if (element instanceof CategoryModel) {
-            if(((CategoryModel)element).getName() == null) {
-                return "Style Name Error";
-            } else {
-                return ((CategoryModel)element).getName();
-            }
-        } else if (element instanceof VisualElementModel) {
-            if (((VisualElementModel)element).getName() == null) {
-                return "Element Name Error";
-            } else                
-                return ((VisualElementModel)element).getName();
-        } 
-        
-        return null;
-    }
-    
-    public void dispose() {
-        for (Iterator i = imageCache.values().iterator(); i.hasNext();) {
-            ((Image) i.next()).dispose();
-        }
-   
-    }
+	public Image getImage(Object element) {
+		if (element instanceof CategoryModel) {
+			ISharedImages images = PlatformUI.getWorkbench().getSharedImages();
+			return images.getImage(ISharedImages.IMG_OBJ_FOLDER);
+		} else {
+			VisualElementModel visualElementModel = ((VisualElementModel) element);
+			String imagename = visualElementModel.getIconFile();
+			String imageId = null;
+			String bundlename = null;
+			if (imagename == null) {
+				org.eclipse.jdt.ui.ISharedImages uiImages = JavaUI.getSharedImages();
+				return uiImages.getImage(org.eclipse.jdt.ui.ISharedImages.IMG_OBJS_CLASS);
+			} else {
+				bundlename = visualElementModel.getContributorBundleName();
+				imageId = bundlename + "/" + imagename;
+			}
+			Image image = (Image) imageCache.get(imageId);
+			if (image == null) {
+				image = CDEPlugin.getImageFromBundle(Platform.getBundle(bundlename), imagename);
+				imageCache.put(imageId, image);
+			}
+			return image;
+		}
+	}
+
+	/*
+	 * @see ILabelProvider#getText(Object)
+	 */
+	public String getText(Object element) {
+		if (element instanceof CategoryModel) {
+			if (((CategoryModel) element).getName() == null) {
+				return "Style Name Error";
+			} else {
+				return ((CategoryModel) element).getName();
+			}
+		} else if (element instanceof VisualElementModel) {
+			if (((VisualElementModel) element).getName() == null) {
+				return "Element Name Error";
+			} else
+				return ((VisualElementModel) element).getName();
+		}
+
+		return null;
+	}
+
+	public void dispose() {
+		for (Iterator i = imageCache.values().iterator(); i.hasNext();) {
+			((Image) i.next()).dispose();
+		}
+
+	}
 }
