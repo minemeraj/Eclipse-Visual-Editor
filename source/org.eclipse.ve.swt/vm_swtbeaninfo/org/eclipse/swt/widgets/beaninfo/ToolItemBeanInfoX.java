3a7055f42efed6280487dab4a56c14f170fafbbc
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-07-27 22:41:27 +0000

    Updated beaninfo for CoolBar/item, ToolBar/item, Menu/item, Item

---
+++ source/org.eclipse.ve.swt/vm_swtbeaninfo/org/eclipse/swt/widgets/beaninfo/ToolItemBeanInfoX.java
@@ -0,0 +1,106 @@
+package org.eclipse.swt.widgets.beaninfo;
+/*
+ * Licensed Material - Property of IBM
+ * (C) Copyright IBM Corp. 2004 - All Rights Reserved.
+ * US Government Users Restricted Rights - Use, duplication or disclosure
+ * restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
+import java.beans.*;
+
+import org.eclipse.swt.SWT;
+
+public class ToolItemBeanInfoX extends IvjBeanInfo {
+
+private static java.util.ResourceBundle ToolItemMessages = java.util.ResourceBundle.getBundle("org.eclipse.swt.widgets.beaninfo.toolitem");  //$NON-NLS-1$
+
+/**
+ * Gets the bean class.
+ * @return java.lang.Class
+ */
+public Class getBeanClass() {
+	return org.eclipse.swt.widgets.ToolItem.class;
+}
+
+public BeanDescriptor getBeanDescriptor() {
+	BeanDescriptor descriptor = new BeanDescriptor(getBeanClass());
+	descriptor.setValue(
+		SweetHelper.STYLE_BITS_ID,
+	    new Object[] [] {
+			{ "style" , ToolItemMessages.getString("ToolItemBeanInfo.StyleBits.Style.Name") , Boolean.FALSE ,  new Object[] { //$NON-NLS-1$ //$NON-NLS-2$
+			    ToolItemMessages.getString("ToolItemBeanInfo.StyleBits.Style.Value.Push") , "org.eclipse.swt.SWT.PUSH" , new Integer(SWT.PUSH) , //$NON-NLS-1$ //$NON-NLS-2$
+				ToolItemMessages.getString("ToolItemBeanInfo.StyleBits.Style.Value.Check") , "org.eclipse.swt.SWT.CHECK" ,  new Integer(SWT.CHECK), 	 //$NON-NLS-1$ //$NON-NLS-2$
+				ToolItemMessages.getString("ToolItemBeanInfo.StyleBits.Style.Value.Radio") , "org.eclipse.swt.SWT.RADIO" , new Integer(SWT.RADIO) , //$NON-NLS-1$ //$NON-NLS-2$
+				ToolItemMessages.getString("ToolItemBeanInfo.StyleBits.Style.Value.Separator") , "org.eclipse.swt.SWT.SEPARATOR" , new Integer(SWT.SEPARATOR) , //$NON-NLS-1$ //$NON-NLS-2$
+				ToolItemMessages.getString("ToolItemBeanInfo.StyleBits.Style.Value.Dropdown") , "org.eclipse.swt.SWT.DROP_DOWN" , new Integer(SWT.DROP_DOWN)	 //$NON-NLS-1$ //$NON-NLS-2$
+			} }
+		}
+	);
+	SweetHelper.mergeSuperclassStyleBits(descriptor);
+	return descriptor;
+}
+
+/**
+ * Return the property descriptors for this bean.
+ * @return java.beans.PropertyDescriptor[]
+ */
+public java.beans.PropertyDescriptor[] getPropertyDescriptors() {
+	try {
+		PropertyDescriptor aDescriptorList[] = {
+			// bounds
+			super.createPropertyDescriptor(getBeanClass(),"bounds", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, ToolItemMessages.getString("boundsDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, ToolItemMessages.getString("boundsSD"), //$NON-NLS-1$
+			}
+			),
+//			// control
+//			super.createPropertyDescriptor(getBeanClass(),"control", new Object[] { //$NON-NLS-1$
+//				DISPLAYNAME, ToolItemMessages.getString("controlDN"), //$NON-NLS-1$
+//				SHORTDESCRIPTION, ToolItemMessages.getString("controlSD"), //$NON-NLS-1$
+//			}
+//			),
+			// disabledImage
+			super.createPropertyDescriptor(getBeanClass(),"disabledImage", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, ToolItemMessages.getString("disabledImageDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, ToolItemMessages.getString("disabledImageSD"), //$NON-NLS-1$
+			}
+			),
+			// enabled
+			super.createPropertyDescriptor(getBeanClass(),"enabled", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, ToolItemMessages.getString("enabledDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, ToolItemMessages.getString("enabledSD"), //$NON-NLS-1$
+			}
+			),
+			// hotImage
+			super.createPropertyDescriptor(getBeanClass(),"hotImage", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, ToolItemMessages.getString("hotImageDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, ToolItemMessages.getString("hotImageSD"), //$NON-NLS-1$
+			}
+			),
+			// selection
+			super.createPropertyDescriptor(getBeanClass(),"selection", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, ToolItemMessages.getString("selectionDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, ToolItemMessages.getString("selectionSD"), //$NON-NLS-1$
+			}
+			),
+			// toolTipText
+			super.createPropertyDescriptor(getBeanClass(),"toolTipText", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, ToolItemMessages.getString("toolTipTextDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, ToolItemMessages.getString("toolTipTextSD"), //$NON-NLS-1$
+			}
+			),
+			// width
+			super.createPropertyDescriptor(getBeanClass(),"width", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, ToolItemMessages.getString("widthDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, ToolItemMessages.getString("widthSD"), //$NON-NLS-1$
+			}
+			),
+		};
+		return aDescriptorList;
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return null;
+}
+
+}
---
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.swt/vm_swtbeaninfo/org/eclipse/swt/widgets/beaninfo/CoolItemBeanInfoX.java
@@ -0,0 +1,85 @@
+package org.eclipse.swt.widgets.beaninfo;
+/*
+ * Licensed Material - Property of IBM
+ * (C) Copyright IBM Corp. 2004 - All Rights Reserved.
+ * US Government Users Restricted Rights - Use, duplication or disclosure
+ * restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+
+import java.beans.BeanDescriptor;
+import java.beans.PropertyDescriptor;
+
+import org.eclipse.swt.SWT;
+
+public class CoolItemBeanInfoX extends IvjBeanInfo {
+
+private static java.util.ResourceBundle CoolItemMessages = java.util.ResourceBundle.getBundle("org.eclipse.swt.widgets.beaninfo.coolitem");  //$NON-NLS-1$
+
+/**
+ * Gets the bean class.
+ * @return java.lang.Class
+ */
+public Class getBeanClass() {
+	return org.eclipse.swt.widgets.CoolItem.class;
+}
+
+/**
+ * Return the property descriptors for this bean.
+ * @return java.beans.PropertyDescriptor[]
+ */
+public java.beans.PropertyDescriptor[] getPropertyDescriptors() {
+	try {
+		PropertyDescriptor aDescriptorList[] = {
+			// bounds
+			super.createPropertyDescriptor(getBeanClass(),"bounds", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, CoolItemMessages.getString("boundsDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, CoolItemMessages.getString("boundsSD"), //$NON-NLS-1$
+			}
+			),
+			// control
+			super.createPropertyDescriptor(getBeanClass(),"control", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, CoolItemMessages.getString("controlDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, CoolItemMessages.getString("controlSD"), //$NON-NLS-1$
+			}
+			),
+			// minimumSize
+			super.createPropertyDescriptor(getBeanClass(),"minimumSize", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, CoolItemMessages.getString("minimumSizeDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, CoolItemMessages.getString("minimumSizeSD"), //$NON-NLS-1$
+			}
+			),
+			// preferredSize
+			super.createPropertyDescriptor(getBeanClass(),"preferredSize", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, CoolItemMessages.getString("preferredSizeDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, CoolItemMessages.getString("preferredSizeSD"), //$NON-NLS-1$
+			}
+			),
+			// size
+			super.createPropertyDescriptor(getBeanClass(),"size", new Object[] { //$NON-NLS-1$
+				DISPLAYNAME, CoolItemMessages.getString("sizeDN"), //$NON-NLS-1$
+				SHORTDESCRIPTION, CoolItemMessages.getString("sizeSD"), //$NON-NLS-1$
+			}
+			),
+		};
+		return aDescriptorList;
+	} catch (Throwable exception) {
+		handleException(exception);
+	};
+	return null;
+}
+
+public BeanDescriptor getBeanDescriptor() {
+	BeanDescriptor descriptor = new BeanDescriptor(getBeanClass());
+	descriptor.setValue(
+		SweetHelper.STYLE_BITS_ID,
+	    new Object[] [] {
+			{ "style" , CoolItemMessages.getString("CoolItemBeanInfo.StyleBits.Style.Name") , Boolean.FALSE ,  new Object[] { //$NON-NLS-1$ //$NON-NLS-2$
+					CoolItemMessages.getString("CoolItemBeanInfo.StyleBits.Style.Value.Dropdown") , "org.eclipse.swt.SWT.DROP_DOWN" , new Integer(SWT.DROP_DOWN)	 //$NON-NLS-1$ //$NON-NLS-2$
+			} }
+		}
+	);
+	SweetHelper.mergeSuperclassStyleBits(descriptor);
+	return descriptor;
+}
+
+}
