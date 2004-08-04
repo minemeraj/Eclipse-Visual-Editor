b028c0232639c126bc0bd6960f39c2107ff3ce21
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-08-04 22:05:08 +0000

    67390 - Table column support on Display thread in remote Vm

--- trunk/source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/TableColumnProxyAdapter.java
+++ trunk/source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/TableColumnProxyAdapter.java
@@ -11,106 +11,63 @@ package org.eclipse.ve.internal.swt;
  *******************************************************************************/
 /*
  *  $RCSfile: TableColumnProxyAdapter.java,v $
- *  $Revision: 1.2 $  $Date: 2004-06-10 18:27:53 $ 
+ *  $Revision: 1.3 $  $Date: 2004-08-04 22:05:08 $ 
  */
 
-import org.eclipse.emf.ecore.*;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 
-import org.eclipse.jem.internal.instantiation.base.IJavaObjectInstance;
+import org.eclipse.jem.internal.instantiation.JavaAllocation;
 import org.eclipse.jem.internal.instantiation.base.JavaInstantiation;
-import org.eclipse.jem.internal.proxy.core.IMethodProxy;
+import org.eclipse.jem.internal.proxy.core.IBeanProxy;
+import org.eclipse.jem.internal.proxy.core.ThrowableProxy;
+import org.eclipse.jem.internal.proxy.swt.DisplayManager;
 
-import org.eclipse.ve.internal.cde.emf.InverseMaintenanceAdapter;
+import org.eclipse.ve.internal.java.core.IBeanProxyDomain;
+import org.eclipse.ve.internal.java.core.JavaEditDomainHelper;
+import org.eclipse.ve.internal.java.core.IAllocationProcesser.AllocationException;
 
-import org.eclipse.ve.internal.java.core.*;
-
-public class TableColumnProxyAdapter extends BeanProxyAdapter {
+public class TableColumnProxyAdapter extends WidgetProxyAdapter {
 
 	
 	protected EStructuralFeature sfModelIndex, sfHeaderValue;
 	protected EReference sfTableColumns;
-	protected IMethodProxy resetHeaderValueProxy;
 	
 	public TableColumnProxyAdapter(IBeanProxyDomain aDomain) {
 		super(aDomain);
 
 		ResourceSet rset = JavaEditDomainHelper.getResourceSet(getBeanProxyDomain().getEditDomain());
-//		sfModelIndex = JavaInstantiation.getSFeature(rset, JFCConstants.SF_TABLECOLUMN_MODELINDEX);		
-//		sfHeaderValue = JavaInstantiation.getSFeature(rset, JFCConstants.SF_TABLECOLUMN_HEADERVALUE);
 		sfTableColumns = JavaInstantiation.getReference(rset, SWTConstants.SF_TABLE_COLUMNS);
 	}
-	
-	/**
-	 * When we validate we need to revalidate the Table as well to cause the image to
-	 * refresh.
-	 */
-	public void validateBeanProxy() {
-	
-		EObject table = ((EObject) getTarget()).eContainer();
-		// If we are on the freeform then container will not be an instance of table.
-		if (table instanceof IJavaObjectInstance) {
-			IBeanProxyHost tableProxyHost = BeanProxyUtilities.getBeanProxyHost((IJavaObjectInstance) table);
-			// The table is a Component so this should refresh the shell and the image
-			tableProxyHost.revalidateBeanProxy();
-		}
-	
-	}
-	/**
-	 * @see org.eclipse.ve.internal.java.core.BeanProxyAdapter#applied(EStructuralFeature, Object, int)
-	 */
-	protected void applied(EStructuralFeature sf, Object newValue, int position) {
-		super.applied(sf, newValue, position);
-		
-		//modelIndexChanged(sf);
-	}
 
-	protected void modelIndexChanged(EStructuralFeature sf) {
-		if (sf == sfModelIndex && getErrorStatus() != ERROR_SEVERE && !getEObject().eIsSet(sfHeaderValue)) {
-			IJavaObjectInstance table = (IJavaObjectInstance) InverseMaintenanceAdapter.getFirstReferencedBy((EObject) getTarget(), sfTableColumns);
-			// Not sure if this would occur - perhaps we could be on the free form although this
-			// is unlikely
-			if (table != null) {
-				IBeanProxyHost tableProxyHost = BeanProxyUtilities.getBeanProxyHost(table);
-				if (tableProxyHost.getErrorStatus() != ERROR_SEVERE) {		
-					// Ask the JTableManager to reset the header value to the default since we've changed the index and have no header value set.
-					//getResetHeaderValueProxy().invokeCatchThrowableExceptions(null, new IBeanProxy[] {tableProxyHost.getBeanProxy(), getBeanProxy()});
-					tableProxyHost.revalidateBeanProxy();
+	/* (non-Javadoc)
+	 * @see org.eclipse.ve.internal.java.core.BeanProxyAdapter#beanProxyAllocation(org.eclipse.jem.internal.instantiation.JavaAllocation)
+	 */
+	protected IBeanProxy beanProxyAllocation(final JavaAllocation allocation) throws AllocationException {
+		try {
+			Object result = invokeSyncExec(new DisplayManager.DisplayRunnable() {
+				public Object run(IBeanProxy displayProxy) throws ThrowableProxy, RunnableException {
+						try {
+							return beanProxyAdapterBeanProxyAllocation(allocation);
+						} catch (AllocationException e) {
+							throw new RunnableException(e);
+						}
 				}
-			}
+			});
+			return (IBeanProxy) result;
+		} catch (ThrowableProxy e) {
+			throw new AllocationException(e);
+		} catch (DisplayManager.DisplayRunnable.RunnableException e) {
+			throw (AllocationException) e.getCause();	// We know it is an allocation exception because that is the only runnable exception we throw.
 		}
 	}
 
-	/**
-	 * @see org.eclipse.ve.internal.java.core.BeanProxyAdapter#canceled(EStructuralFeature, Object, int)
+	/*
+	 * Use to call BeanProxyAdapter's beanProxyAllocation.
 	 */
-	protected void canceled(EStructuralFeature sf, Object oldValue, int position) {
-		super.canceled(sf, oldValue, position);
-		
-		//modelIndexChanged(sf);
+	protected IBeanProxy beanProxyAdapterBeanProxyAllocation(JavaAllocation allocation) throws AllocationException {
+		return super.beanProxyAllocation(allocation);
 	}
 	
-	protected IMethodProxy getResetHeaderValueProxy() {
-		if (resetHeaderValueProxy == null) {
-			resetHeaderValueProxy = getBeanProxy()
-				.getProxyFactoryRegistry()
-				.getBeanTypeProxyFactory()
-				.getBeanTypeProxy("org.eclipse.ve.internal.jfc.vm.JTableManager")	//$NON-NLS-1$
-				.getMethodProxy("resetHeaderValue", new String[] {"javax.swing.JTable", "javax.swing.table.TableColumn"});	//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		}
-		return resetHeaderValueProxy;
-	}	
-
-	/**
-	 * @see org.eclipse.ve.internal.java.core.IBeanProxyHost#releaseBeanProxy()
-	 */
-	public void releaseBeanProxy() {
-		super.releaseBeanProxy();
-		
-		if (resetHeaderValueProxy != null) {
-			resetHeaderValueProxy.getProxyFactoryRegistry().releaseProxy(resetHeaderValueProxy);
-			resetHeaderValueProxy = null;
-		}		
-	}
-
 }
