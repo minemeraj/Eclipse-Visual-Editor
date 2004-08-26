5496c0f4b1b6a2a04a9fc8d72f328dae4c3d17b4
jmyers <jmyers@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-08-26 15:54:59 +0000

    72700 - Fixed odd behaviors of Spinner by using a text verifier rather than modify listeners

---
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.java.core/core/org/eclipse/ve/internal/java/core/NumberTextVerifier.java
@@ -0,0 +1,112 @@
+package org.eclipse.ve.internal.java.core;
+
+/*******************************************************************************
+ * Copyright (c)  2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*
+ *  $RCSfile: NumberTextVerifier.java,v $
+ *  $Revision: 1.1 $  $Date: 2004-08-26 15:54:59 $ 
+ */
+
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
+import org.eclipse.swt.widgets.Text;
+
+public class NumberTextVerifier implements VerifyListener {
+	
+	private int newValue;
+	private int defaultValue = 0;
+	private int min = Integer.MIN_VALUE;
+	private int max = Integer.MAX_VALUE;
+	
+	/**
+	 * The default value is the value of the verifier when
+	 * no text is entered, or if the text is only ('-') when
+	 * negative values are allowed.  The default is 0.
+	 * 
+	 * @return Returns the defaultValue.
+	 */
+	public int getDefaultValue() {
+		return defaultValue;
+	}
+	/**
+	 * @param defaultValue The defaultValue to set.
+	 */
+	public void setDefaultValue(int defaultValue) {
+		if (defaultValue < min || defaultValue > max) return;
+		this.defaultValue = defaultValue;
+	}
+	
+	/**
+	 * Get the latest valid value from the verifier.  The value
+	 * will be an integer between min and max.
+	 * 
+	 * @return Returns the newValue.
+	 */
+	public int getNewValue() {
+		return newValue;
+	}
+
+	/**
+	 * @return Returns the maximum valid integer.
+	 */
+	public int getMax() {
+		return max;
+	}
+	/**
+	 * Set the maximum valid integer value this verifier should accept.
+	 * @param max The max to set.
+	 */
+	public void setMax(int max) {
+		this.max = max;
+		if (defaultValue > max) {
+			defaultValue = max;
+		}
+	}
+	/**
+	 * @return Returns the minimum valid integer.
+	 */
+	public int getMin() {
+		return min;
+	}
+	/**
+	 * Set the minimum valid integer value this verifier should accept.
+	 * @param min The min to set.
+	 */
+	public void setMin(int min) {
+		this.min = min;
+		if (defaultValue < min) {
+			defaultValue = min;
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.swt.events.VerifyListener#verifyText(org.eclipse.swt.events.VerifyEvent)
+	 */
+	public void verifyText(VerifyEvent e) {
+		if (e.widget instanceof Text) {
+			StringBuffer textbuf = new StringBuffer(((Text)e.widget).getText());
+			textbuf.replace(e.start, e.end, e.text);
+			if (textbuf.length() == 0 || (min < 0 && textbuf.toString().equals("-"))) {
+				newValue = defaultValue;
+				return;
+			}
+			try { 
+				int value = Integer.parseInt(textbuf.toString());
+				if (value < min || value > max) {
+					e.doit = false;
+				}
+				newValue = value;
+			} catch (NumberFormatException ex) {
+				e.doit = false;
+			}
+		}
+	}
+}
