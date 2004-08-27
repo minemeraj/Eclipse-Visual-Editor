7915dc8a76abb7368552eabb1d238c1e08881107
jmyers <jmyers@f56db7ec-86c8-4192-a518-b33116ffafde>
2004-08-27 17:34:17 +0000

    72264 - Prevent dropping JMenuBars on any container that does not have a setJMenuBar() method.

---
+++ trunk/source/org.eclipse.ve.jfc/jfc/org/eclipse/ve/internal/jfc/core/JMenuBarRootPaneOnlyModelAdapter.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2001, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ve.internal.jfc.core;
+/*
+ *  $RCSfile: JMenuBarRootPaneOnlyModelAdapter.java,v $
+ *  $Revision: 1.1 $  $Date: 2004-08-27 17:34:17 $ 
+ */
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.jem.java.JavaClass;
+import org.eclipse.ve.internal.cde.core.IContainmentHandler;
+
+public class JMenuBarRootPaneOnlyModelAdapter extends ComponentModelAdapter implements IContainmentHandler {
+
+	public JMenuBarRootPaneOnlyModelAdapter(Object model) {
+		super(model);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ve.internal.cde.core.IContainmentHandler#isParentValid(java.lang.Object)
+	 */
+	public boolean isParentValid(Object parent) {
+		if (parent instanceof EObject) {
+			EClass parentClass = ((EObject)parent).eClass();
+			if (parentClass instanceof JavaClass) {
+				EStructuralFeature sf = parentClass.getEStructuralFeature("JMenuBar"); //$NON-NLS-1$
+				if (sf != null) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
