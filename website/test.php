8cefc9013e5755747f9ccf4f8348ed9384588ee5
droy <droy@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-01-06 15:43:27 +0000

    *** empty log message ***

---
+++ website/test.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Created on Jan 6, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+
+echo "Hello, world!";
+
+if (!($f2=fopen("tests.new","w+")))
+                  exit("Unable to open file.");
+fwrite($f2,"Testing\n");
+fclose($f2);
