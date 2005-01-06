2a79ec27b65dd0b3a54b561f119cc5aae6666fba
droy <droy@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-01-06 16:05:08 +0000

    *** empty log message ***

--- website/test.php
+++ website/test.php
@@ -9,13 +9,13 @@
 
 echo "Hello, world!";
 
-if (!($f2=fopen("public/tests.new","w+")))
+if (!($f2=fopen("public/tests.txt","w+")))
                   exit("Unable to open file.");
 fwrite($f2,"Testing\n");
 fclose($f2);
 
 
-if (!($f2=fopen("public/tests.new","r")))
+if (!($f2=fopen("public/tests.txt","r")))
                   exit("Unable to open file.");
 $string = fread($f2,8);
 fclose($f2);
