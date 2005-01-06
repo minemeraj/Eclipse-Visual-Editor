d96f1655bb220c88e913f4eda5b1eef5242d31be
droy <droy@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-01-06 15:52:16 +0000

    *** empty log message ***

--- website/test.php
+++ website/test.php
@@ -5,7 +5,7 @@
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
-?>
+
 
 echo "Hello, world!";
 
@@ -13,3 +13,5 @@ if (!($f2=fopen("tests.new","w+")))
                   exit("Unable to open file.");
 fwrite($f2,"Testing\n");
 fclose($f2);
+
+?>
