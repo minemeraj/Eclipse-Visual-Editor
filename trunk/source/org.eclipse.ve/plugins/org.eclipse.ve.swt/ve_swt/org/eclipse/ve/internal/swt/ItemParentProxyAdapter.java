b93dc4ab608d8ec7251de08ae49b060fc94092b2
winchest <winchest@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-03-07 21:28:39 +0000

    Bugzilla 86932

---
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ItemParentProxyAdapter.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*
+ *  $RCSfile: ItemParentProxyAdapter.java,v $
+ *  $Revision: 1.1 $  $Date: 2005-03-07 21:28:39 $ 
+ */
+package org.eclipse.ve.internal.swt;
+
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.jem.internal.instantiation.base.IJavaInstance;
+import org.eclipse.ve.internal.java.core.BeanProxyUtilities;
+import org.eclipse.ve.internal.java.core.IBeanProxyDomain;
+import org.eclipse.ve.internal.java.core.IBeanProxyHost;
+ 
+/**
+ * 
+ * @since 1.0.2
+ */
+public class ItemParentProxyAdapter extends CompositeProxyAdapter {
+
+    /**
+     * @param domain
+     * 
+     * @since 1.0.2
+     */
+    public ItemParentProxyAdapter(IBeanProxyDomain domain) {
+        super(domain);                
+    }
+    /* (non-Javadoc)
+     * @see org.eclipse.ve.internal.swt.CompositeProxyAdapter#applied(org.eclipse.emf.ecore.EStructuralFeature, java.lang.Object, int)
+     */
+    protected void applied(EStructuralFeature as, Object newValue, int position) {
+        // To create the item just instantiate it.  It will detect us as its parent because that is part
+        // of its parse tree allocation statement
+        if (as.getName().equals("items")) {
+            IBeanProxyHost valueProxyHost = BeanProxyUtilities.getBeanProxyHost((IJavaInstance)newValue);
+            valueProxyHost.instantiateBeanProxy();
+		} else {
+		    super.applied(as, newValue, position);
+		}
+    }
+
+}
