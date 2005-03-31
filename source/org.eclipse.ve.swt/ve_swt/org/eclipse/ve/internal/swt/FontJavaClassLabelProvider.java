39f5ca1e10b3ea1b06b644a92f06bd98cf3f3747
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-03-31 00:54:39 +0000

    88530 -  handle parse tree allocations for showing JFace fonts

--- source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/FontJavaClassLabelProvider.java
+++ source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/FontJavaClassLabelProvider.java
@@ -10,39 +10,63 @@
  *******************************************************************************/
 /*
  *  $RCSfile: FontJavaClassLabelProvider.java,v $
- *  $Revision: 1.6 $  $Date: 2005-03-29 16:17:15 $ 
+ *  $Revision: 1.7 $  $Date: 2005-03-31 00:54:39 $ 
  */
 package org.eclipse.ve.internal.swt;
 
+import org.eclipse.jface.viewers.LabelProvider;
+
+import org.eclipse.jem.internal.instantiation.*;
 import org.eclipse.jem.internal.instantiation.base.IJavaInstance;
 import org.eclipse.jem.internal.proxy.core.*;
-import org.eclipse.jface.viewers.LabelProvider;
 
 import org.eclipse.ve.internal.java.core.BeanProxyUtilities;
 
 public class FontJavaClassLabelProvider extends LabelProvider {
-	
-public String getText(Object element){	
-	if (element instanceof IJavaInstance){
-		return getText((IJavaInstance)element);
-	} else {
+
+	public String getText(Object element) {
+		if (element instanceof IJavaInstance) {
+			return getText((IJavaInstance) element);
+		} else {
+			return "";
+		}
+	}
+
+	public static String getText(IJavaInstance element) {
+		/*
+		 * For JFace fonts, show the label based on the ParseTreeAllocation information
+		 */
+		if (element != null && element.getAllocation() instanceof ParseTreeAllocation) {
+			ParseTreeAllocation ptAlloc = (ParseTreeAllocation) element.getAllocation();
+			PTExpression exp = ptAlloc.getExpression();
+			if (exp instanceof PTMethodInvocation && ((PTMethodInvocation) exp).getReceiver() instanceof PTMethodInvocation
+					&& ((PTMethodInvocation) ((PTMethodInvocation) exp).getReceiver()).getName().equals("getFontRegistry")) { //$NON-NLS-1$
+				PTExpression arg = (PTExpression) ((PTMethodInvocation) exp).getArguments().get(0);
+				StringBuffer fontLabelBuffer = new StringBuffer();
+				fontLabelBuffer.append("JFace, "); //$NON-NLS-1$
+				fontLabelBuffer.append(((PTMethodInvocation) exp).getName());
+				fontLabelBuffer.append(','); //$NON-NLS-1$
+				fontLabelBuffer.append(' '); //$NON-NLS-1$
+				if (arg instanceof PTFieldAccess) {
+					fontLabelBuffer.append(((PTFieldAccess) arg).getField());
+				}
+				return fontLabelBuffer.toString();
+			}
+		}
+		try {
+			// Get the description of the Font. This comes from getting the FontData
+			// and its name, style and size and formatting these. To save lots of
+			// target VM traffic a single static helper method exists on the utility class
+			// org.eclipse.ve.internal.swt.targetvm.Environment.getFontLabel(Font aFont)
+			IBeanProxy fontBeanProxy = BeanProxyUtilities.getBeanProxy((IJavaInstance) element);
+			if (fontBeanProxy == null) return ""; //$NON-NLS-1$ 
+			IBeanTypeProxy environmentBeanTypeProxy = fontBeanProxy.getProxyFactoryRegistry().getBeanTypeProxyFactory().getBeanTypeProxy(
+					"org.eclipse.ve.internal.swt.targetvm.Environment"); //$NON-NLS-1$
+			IMethodProxy getFontLabelMethodProxy = environmentBeanTypeProxy.getMethodProxy("getFontLabel", new IBeanTypeProxy[] { fontBeanProxy.getTypeProxy()}); //$NON-NLS-1$
+			IStringBeanProxy fontLabelBeanProxy = (IStringBeanProxy) getFontLabelMethodProxy.invoke(environmentBeanTypeProxy, fontBeanProxy);
+			return fontLabelBeanProxy.stringValue();
+		} catch (ThrowableProxy exc) {
+		}
 		return "";
 	}
-}
-public static String getText(IJavaInstance element){
-	try{
-		// Get the description of the Font.  This comes from getting the FontData
-		// and its name, style and size and formatting these.  To save lots of 
-		// target VM traffic a single static helper method exists on the utility class
-		// org.eclipse.ve.internal.swt.targetvm.Environment.getFontLabel(Font aFont)
-		IBeanProxy fontBeanProxy = BeanProxyUtilities.getBeanProxy((IJavaInstance)element);
-		if (fontBeanProxy == null) return "";
-		IBeanTypeProxy environmentBeanTypeProxy = fontBeanProxy.getProxyFactoryRegistry().getBeanTypeProxyFactory().getBeanTypeProxy("org.eclipse.ve.internal.swt.targetvm.Environment");
-		IMethodProxy getFontLabelMethodProxy = environmentBeanTypeProxy.getMethodProxy("getFontLabel", new IBeanTypeProxy[] {fontBeanProxy.getTypeProxy()});
-		IStringBeanProxy fontLabelBeanProxy = (IStringBeanProxy) getFontLabelMethodProxy.invoke(environmentBeanTypeProxy,fontBeanProxy);
-		return fontLabelBeanProxy.stringValue();			
-	} catch (ThrowableProxy exc){
-	}
-	return "";	
-}
 }
