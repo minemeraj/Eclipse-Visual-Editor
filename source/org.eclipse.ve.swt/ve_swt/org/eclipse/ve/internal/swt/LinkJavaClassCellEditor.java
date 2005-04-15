ae47d02d482805b7cba0605daae374daf4591794
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-04-15 18:55:53 +0000

    91526 - code from Jon - launch info message on async thread to handle situations where save prompt is coming up when editor is closing

--- source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/LinkJavaClassCellEditor.java
+++ source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/LinkJavaClassCellEditor.java
@@ -10,43 +10,41 @@
  *******************************************************************************/
 /*
  *  $RCSfile: LinkJavaClassCellEditor.java,v $
- *  $Revision: 1.1 $  $Date: 2005-04-11 17:31:49 $ 
+ *  $Revision: 1.2 $  $Date: 2005-04-15 18:55:53 $ 
  */
 package org.eclipse.ve.internal.swt;
 
 import java.text.MessageFormat;
 
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.*;
+
 import org.eclipse.jem.internal.instantiation.base.IJavaInstance;
 import org.eclipse.jem.internal.proxy.core.IStringBeanProxy;
 import org.eclipse.jem.java.JavaRefFactory;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Link;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ve.internal.java.core.BeanProxyUtilities;
-import org.eclipse.ve.internal.java.core.BeanUtilities;
-import org.eclipse.ve.internal.java.core.DefaultJavaClassCellEditor;
-import org.eclipse.ve.internal.java.core.JavaEditDomainHelper;
+
+import org.eclipse.ve.internal.java.core.*;
 
 public class LinkJavaClassCellEditor extends DefaultJavaClassCellEditor {
-	
+
 	private String value;
 	private boolean infoMessageDisplayed = false;
 
-	public LinkJavaClassCellEditor(Composite aComposite){
+	public LinkJavaClassCellEditor(Composite aComposite) {
 		super(aComposite);
 	}
-	
+
 	protected String getJavaInitializationString(String aString) {
 		return BeanUtilities.createStringInitString(aString);
 	}
 
 	protected String doGetString(Object value) {
 		if (isInstance(value)) {
-			return ((IStringBeanProxy) BeanProxyUtilities.getBeanProxy((IJavaInstance) value, JavaEditDomainHelper.getResourceSet(fEditDomain))).stringValue();
+			return ((IStringBeanProxy) BeanProxyUtilities.getBeanProxy((IJavaInstance) value, JavaEditDomainHelper.getResourceSet(fEditDomain)))
+					.stringValue();
 		} else if (value != null) {
-			// Since this is not a string value, the text widget is disabled, 
+			// Since this is not a string value, the text widget is disabled,
 			// just return the toString of this object.
 			return BeanProxyUtilities.getBeanProxy((IJavaInstance) value, JavaEditDomainHelper.getResourceSet(fEditDomain)).toBeanString();
 		} else
@@ -58,18 +56,17 @@ public class LinkJavaClassCellEditor extends DefaultJavaClassCellEditor {
 		this.value = value;
 		Shell shell = new Shell();
 		Link link = null;
-		try{
+		try {
 			link = new Link(shell, SWT.NONE);
-			if("".equals(value)) value = null;
+			if ("".equals(value))value = null; //$NON-NLS-1$
 			link.setText(value);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			this.infoMessageDisplayed = true;
-			return SWTMessages.getString("LinkJavaClassCellEditor.NoLink_ERROR_");
+			return SWTMessages.getString("LinkJavaClassCellEditor.NoLink_ERROR_"); //$NON-NLS-1$
 		}
 		return null;
 	}
-	
+
 	public void setData(Object data) {
 		super.setData(data);
 		setJavaType(JavaRefFactory.eINSTANCE.reflectType("java.lang.String", JavaEditDomainHelper.getResourceSet(fEditDomain))); //$NON-NLS-1$
@@ -77,14 +74,21 @@ public class LinkJavaClassCellEditor extends DefaultJavaClassCellEditor {
 
 	protected void fireApplyEditorValue() {
 		super.fireApplyEditorValue();
-		if( !this.infoMessageDisplayed && this.isDirty() && this.value != null){
-			this.infoMessageDisplayed = true;
-			int openA = this.value.indexOf("<a>");
-			int closeA = this.value.indexOf("</a>");
-			if( (openA == -1) || (closeA == -1) || (openA > closeA) ){
-				String message = MessageFormat.format(SWTMessages.getString("LinkJavaClassCellEditor.NoLink_INFO_"), new Object[] {this.value}); //$NON-NLS-1$
-				MessageDialog.openInformation(null,SWTMessages.getString("LinkJavaClassCellEditor.NoLinkInfoTitle"), message); //$NON-NLS-1$
-			}
+		int openA = value.indexOf("<a>"); //$NON-NLS-1$
+		int closeA = value.indexOf("</a>"); //$NON-NLS-1$
+		if ((openA == -1) || (closeA == -1) || (openA > closeA) && 
+				(!infoMessageDisplayed && isDirty() && value != null)) {
+			infoMessageDisplayed = true;
+			final Shell shell = Display.getCurrent().getActiveShell();
+			Display.getCurrent().asyncExec(new Runnable() {
+
+				public void run() {
+					if (!shell.isDisposed()) {
+						String message = MessageFormat.format(SWTMessages.getString("LinkJavaClassCellEditor.NoLink_INFO_"), new Object[] { value}); //$NON-NLS-1$
+						MessageDialog.openInformation(shell, SWTMessages.getString("LinkJavaClassCellEditor.NoLinkInfoTitle"), message); //$NON-NLS-1$
+					}
+				}
+			});
 		}
 	}
 }
