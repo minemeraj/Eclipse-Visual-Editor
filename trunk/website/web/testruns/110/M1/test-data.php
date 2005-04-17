9d3fd63d97895b3585eaa7db2f854458fdb60089
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-04-17 02:46:09 +0000

    *** empty log message ***

--- trunk/website/web/testruns/110/M1/test-data.php
+++ trunk/website/web/testruns/110/M1/test-data.php
@@ -48,8 +48,10 @@ if(strlen($action)<1){
 	if(chdir($DESTDIR)){
 		echo "<li>source = $sourcefile";
 		echo "<li>destination = $DESTFILE";
-		echo "<li>is readble: " . is_readable($DESTDIR);
+		echo "<li>is readble: " . is_readable($sourcefile);
 		echo "<li>is writeable: " . is_writeable($DESTDIR);
+		echo "<li>dest. exists?: " . file_exists($DESTFILE);
+		echo "<li>src. exists?: " . file_exists($sourcefile);
 		if(!copy($sourcefile, $DESTFILE)){
 			exit("<h1>Cannot restore file via copy</h1>");
 		}else{
