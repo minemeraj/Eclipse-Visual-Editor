3333fffad0a290532ed2f97e360e14b0f649adef
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-04-17 03:13:41 +0000

    Correct setup for mirror protection

--- trunk/website/web/testruns/110/M1/resultit2.php
+++ trunk/website/web/testruns/110/M1/resultit2.php
@@ -7,11 +7,11 @@
 </head>
 <body>
     <?php        
-       if (!($f=fopen("/home/data/httpd/download.eclipse.org/tools/ve/web/testruns/110/M1/tests-ve1.1m1.txt","r"))) 
+       if (!($f=fopen("/home/data/httpd/www.eclipse.org/html/vep/writable/testruns/110/M1/tests-ve1.1m1.txt","r"))) 
                   exit("Unable to open file.");
        if (!flock($f, LOCK_EX)) 
                   exit ("Site is busy") ;
-       if (!($f2=fopen("/home/data/httpd/download.eclipse.org/tools/ve/web/testruns/110/M1/tests-ve1.1m1.txt.new","w+"))) 
+       if (!($f2=fopen("/home/data/httpd/www.eclipse.org/html/vep/writable/testruns/110/M1/tests-ve1.1m1.txt.new","w+"))) 
                   exit("Unable to open file.");  
        while ($testinfo = fscanf ($f, "%s\t%s\t%s\t%s\t%[a-zA-Z0-9,. \\'';;~~!!@@##$$%%&&**(())--==++__]\n")) {
               list ($tst, $url, $tester, $status, $description) = $testinfo;
@@ -20,7 +20,7 @@
                   $description=$_POST["Comments"];
                   // ve-dev@eclipse.org 
 //                  mail("ve-dev@eclipse.org, ".$_POST["Email"], $_POST["Test"].": ".$_POST["Result"].".", "VisualEditor, M8 TestPass\n\tTestcase " . $_POST["Test"] . " status=".$_POST["Result"]." by ". $_POST["Email"]."\nComments:\n".$description.".", "From: VEproject@{$_SERVER['SERVER_NAME']}");                  
-                  mail("ve-dev@eclipse.org, ".$_POST["Email"], $_POST["Test"].": ".$_POST["Result"].".", "VisualEditor, M8 TestPass\n\tTestcase " . $_POST["Test"] . " status=".$_POST["Result"]." by ". $_POST["Email"]."\nComments:\n".$description.".", "From: gmendel@us.ibm.com");                  
+                  mail("sgunturi@us.ibm.com, ".$_POST["Email"], $_POST["Test"].": ".$_POST["Result"].".", "VisualEditor, M8 TestPass\n\tTestcase " . $_POST["Test"] . " status=".$_POST["Result"]." by ". $_POST["Email"]."\nComments:\n".$description.".", "From: gmendel@us.ibm.com");                  
               }
               $out=sprintf("%s\t%s\t%s\t%s\t%s\n", $tst, $url, $tester, $status, $description);
               fwrite($f2,$out);
@@ -28,9 +28,9 @@
        flock($f, LOCK_UN);
        fclose($f);
        fclose($f2);
-       copy("/home/data/httpd/download.eclipse.org/tools/ve/web/testruns/110/M1/tests-ve1.1m1.txt.new", "/home/data/httpd/download.eclipse.org/tools/ve/web/testruns/110/M1/tests-ve1.1m1.txt");
-       unlink("/home/data/httpd/download.eclipse.org/tools/ve/web/testruns/110/M1/tests-ve1.1m1.txt.new");
-       chmod ("/home/data/httpd/download.eclipse.org/tools/ve/web/testruns/110/M1/tests-ve1.1m1.txt","ugo+rw");      
+       copy("/home/data/httpd/www.eclipse.org/html/vep/writable/testruns/110/M1/tests-ve1.1m1.txt.new", "/home/data/httpd/www.eclipse.org/html/vep/writable/testruns/110/M1/tests-ve1.1m1.txt");
+       unlink("/home/data/httpd/www.eclipse.org/html/vep/writable/testruns/110/M1/tests-ve1.1m1.txt.new");
+       chmod ("/home/data/httpd/www.eclipse.org/html/vep/writable/testruns/110/M1/tests-ve1.1m1.txt","ugo+rw");      
      ?>			
 </body>
 
