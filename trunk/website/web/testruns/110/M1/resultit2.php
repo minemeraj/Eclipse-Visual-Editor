535245e33d80dc21ef9c1331711a097e88f55cc2
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-04-17 04:37:51 +0000

    *** empty log message ***

--- trunk/website/web/testruns/110/M1/resultit2.php
+++ trunk/website/web/testruns/110/M1/resultit2.php
@@ -1,6 +1,6 @@
-<?php header("Location: http://".$_SERVER['HTTP_HOST']
-                       .dirname($_SERVER['PHP_SELF'])
-                       ."/index.php"); ?>
+<?php 
+//header("Location: http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/index.php"); 
+?>
 <html>
 <head>
 <title></title>
