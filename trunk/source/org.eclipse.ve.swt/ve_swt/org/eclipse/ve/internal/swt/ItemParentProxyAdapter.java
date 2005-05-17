5312dd6ef558879c7d311500b6adc32a2a48c24a
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-05-17 18:27:09 +0000

    95572 - release all the items when the parent is released

--- trunk/source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ItemParentProxyAdapter.java
+++ trunk/source/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ItemParentProxyAdapter.java
@@ -10,42 +10,63 @@
  *******************************************************************************/
 /*
  *  $RCSfile: ItemParentProxyAdapter.java,v $
- *  $Revision: 1.2 $  $Date: 2005-04-05 20:11:45 $ 
+ *  $Revision: 1.3 $  $Date: 2005-05-17 18:27:09 $ 
  */
 package org.eclipse.ve.internal.swt;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.jem.internal.instantiation.base.IJavaInstance;
+import org.eclipse.jem.internal.instantiation.base.IJavaObjectInstance;
+
 import org.eclipse.ve.internal.java.core.BeanProxyUtilities;
 import org.eclipse.ve.internal.java.core.IBeanProxyDomain;
 import org.eclipse.ve.internal.java.core.IBeanProxyHost;
- 
+
 /**
  * 
  * @since 1.0.2
  */
 public class ItemParentProxyAdapter extends CompositeProxyAdapter {
 
-    /**
-     * @param domain
-     * 
-     * @since 1.0.2
-     */
-    public ItemParentProxyAdapter(IBeanProxyDomain domain) {
-        super(domain);                
-    }
-    /* (non-Javadoc)
-     * @see org.eclipse.ve.internal.swt.CompositeProxyAdapter#applied(org.eclipse.emf.ecore.EStructuralFeature, java.lang.Object, int)
-     */
-    protected void applied(EStructuralFeature as, Object newValue, int position) {
-        // To create the item just instantiate it.  It will detect us as its parent because that is part
-        // of its parse tree allocation statement
-        if (as.getName().equals("items")) { //$NON-NLS-1$
-            IBeanProxyHost valueProxyHost = BeanProxyUtilities.getBeanProxyHost((IJavaInstance)newValue);
-            valueProxyHost.instantiateBeanProxy();
+	/**
+	 * @param domain
+	 * 
+	 * @since 1.0.2
+	 */
+	public ItemParentProxyAdapter(IBeanProxyDomain domain) {
+		super(domain);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ve.internal.swt.CompositeProxyAdapter#applied(org.eclipse.emf.ecore.EStructuralFeature, java.lang.Object, int)
+	 */
+	protected void applied(EStructuralFeature as, Object newValue, int position) {
+		// To create the item just instantiate it. It will detect us as its parent because that is part
+		// of its parse tree allocation statement
+		if (as.getName().equals("items")) { //$NON-NLS-1$
+			IBeanProxyHost valueProxyHost = BeanProxyUtilities.getBeanProxyHost((IJavaInstance) newValue);
+			valueProxyHost.instantiateBeanProxy();
 		} else {
-		    super.applied(as, newValue, position);
+			super.applied(as, newValue, position);
 		}
-    }
+	}
+
+	public void releaseBeanProxy() {
+		if (isBeanProxyInstantiated()) {
+			List controls = (List) ((IJavaObjectInstance) getTarget()).eGet(getJavaObject().eClass().getEStructuralFeature("items"));
+			Iterator iter = controls.iterator();
+			while (iter.hasNext()) {
+				IBeanProxyHost value = BeanProxyUtilities.getBeanProxyHost((IJavaInstance) iter.next());
+				if (value != null)
+					value.releaseBeanProxy();
+			}
+		}
+		super.releaseBeanProxy();
+	}
 
 }
