518c3efbfcade8a8c92fc5210570e5a52e9f2246
winchest <winchest@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-06-03 15:10:56 +0000

    Bugzilla 96416

--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/jface/ViewPartTreeEditPart.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/jface/ViewPartTreeEditPart.java
@@ -10,13 +10,19 @@
  *******************************************************************************/
 /*
  *  $RCSfile: ViewPartTreeEditPart.java,v $
- *  $Revision: 1.2 $  $Date: 2005-05-11 22:41:38 $ 
+ *  $Revision: 1.3 $  $Date: 2005-06-03 15:06:36 $ 
  */
 package org.eclipse.ve.internal.jface;
 
 import java.util.*;
 
+import org.eclipse.emf.common.notify.*;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
 import org.eclipse.jem.internal.instantiation.base.IJavaInstance;
+
+import org.eclipse.ve.internal.cde.emf.EditPartAdapterRunnable;
 
 import org.eclipse.ve.internal.java.core.BeanUtilities;
 import org.eclipse.ve.internal.java.core.JavaBeanTreeEditPart;
@@ -26,13 +32,15 @@ import org.eclipse.ve.internal.swt.SwtPlugin;
 
 public class ViewPartTreeEditPart extends JavaBeanTreeEditPart {
 	
+	private EStructuralFeature sf_delegate_control;
+
 	public ViewPartTreeEditPart(Object aModel) {
 		super(aModel);
 	}
 	
 	protected List getChildJavaBeans() {
 		List result = new ArrayList(1);
-		IJavaInstance delegate_control = BeanUtilities.getFeatureValue((IJavaInstance)getModel(),SwtPlugin.DELEGATE_CONTROL);
+		IJavaInstance delegate_control = BeanUtilities.getFeatureValue((IJavaInstance)getModel(),sf_delegate_control);
 		if(delegate_control != null){
 			result.add(delegate_control);
 			return result;			
@@ -40,5 +48,32 @@ public class ViewPartTreeEditPart extends JavaBeanTreeEditPart {
 			return Collections.EMPTY_LIST;
 		}
 	}
+	
+	public void setModel(Object aModel) {
+		sf_delegate_control = ((EObject)aModel).eClass().getEStructuralFeature(SwtPlugin.DELEGATE_CONTROL);
+		super.setModel(aModel);
+	}
+	
+	public void activate() {
+		super.activate();
+		((EObject) getModel()).eAdapters().add(viewPartAdapter);
+	}
 
+	public void deactivate() {
+		super.deactivate();
+		((EObject) getModel()).eAdapters().remove(viewPartAdapter);
+	}	
+	
+	protected Adapter viewPartAdapter = new EditPartAdapterRunnable(this) {
+		public void doRun() {
+			if (isActive())
+				refreshChildren();
+		}
+
+		public void notifyChanged(Notification notification) {
+			if (notification.getFeature() == sf_delegate_control) {
+				queueExec(ViewPartTreeEditPart.this, "DELEGATE_CONTROL"); //$NON-NLS-1$
+			}
+		}
+	};
 }
