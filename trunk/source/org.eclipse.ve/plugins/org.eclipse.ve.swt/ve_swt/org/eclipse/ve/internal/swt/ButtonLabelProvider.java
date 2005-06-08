0affff415376f0bbcddc268f85028ea4cc9aeb66
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-06-08 20:17:46 +0000

    Created class ButtonLabelProvider to provide different images for Button,CheckBox, and RadioButton in the Java Beans View.

---
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/ButtonLabelProvider.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*
+ *  $RCSfile: ButtonLabelProvider.java,v $
+ *  $Revision: 1.1 $  $Date: 2005-06-08 20:17:46 $ 
+ */
+package org.eclipse.ve.internal.swt;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+
+import org.eclipse.jem.internal.instantiation.base.IJavaObjectInstance;
+
+import org.eclipse.ve.internal.java.core.BeanProxyUtilities;
+import org.eclipse.ve.internal.java.core.DefaultLabelProviderWithNameAndAttribute;
+ 
+
+public class ButtonLabelProvider extends DefaultLabelProviderWithNameAndAttribute {
+
+	private Image image = null;
+	
+	private final String CHECK_BOX = "platform:/plugin/org.eclipse.ve.swt/icons/full/clcl16/checkbox_obj.gif"; //$NON-NLS-1$
+		
+	private final String RADIO_BUTTON = "platform:/plugin/org.eclipse.ve.swt/icons/full/clcl16/radiobutton_obj.gif"; //$NON-NLS-1$
+	
+	public Image getImage(Object element) {
+		if (element instanceof IJavaObjectInstance) {
+			URL iconURL = null;
+
+			WidgetProxyAdapter widgetProxyAdapter = 
+				(WidgetProxyAdapter) BeanProxyUtilities.getBeanProxyHost((IJavaObjectInstance) element);
+			
+			int style = widgetProxyAdapter.getStyle();
+			
+	        if(( style & SWT.CHECK) != 0){
+				try{
+					iconURL = new URL(CHECK_BOX);
+				} catch(MalformedURLException mue){}
+	        } else if (( style & SWT.RADIO) != 0){
+				try{
+					iconURL = new URL(RADIO_BUTTON);
+				} catch(MalformedURLException mue){}		        	
+	        }
+			if ( iconURL != null) {
+				ImageDescriptor imageDescriptor = ImageDescriptor.createFromURL(iconURL);
+				image = imageDescriptor.createImage();
+				return image;
+			}
+		} 
+		return super.getImage(element);	
+	}
+
+	public void dispose() {
+		if(image != null){
+			image.dispose();
+		}
+		super.dispose();
+	}
+}
