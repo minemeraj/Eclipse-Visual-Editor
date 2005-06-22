a64d084355af483a52482db469fabbf7dc4ecdbf
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-06-22 16:43:51 +0000

    86479 : [perf] Convert msg. bundles to use instance variables

--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.jfc/jfccodegen/org/eclipse/ve/internal/jfc/codegen/JFCCodegenMessages.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.jfc/jfccodegen/org/eclipse/ve/internal/jfc/codegen/JFCCodegenMessages.java
@@ -1,37 +1,29 @@
 /*******************************************************************************
- * Copyright (c) 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-/*
- *  $RCSfile: JFCCodegenMessages.java,v $
- *  $Revision: 1.1 $  $Date: 2005-06-21 22:28:36 $ 
- */
 package org.eclipse.ve.internal.jfc.codegen;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import org.eclipse.osgi.util.NLS;
 
-public class JFCCodegenMessages {
+public final class JFCCodegenMessages extends NLS {
 
-	private static final String BUNDLE_NAME = "org.eclipse.ve.internal.jfc.codegen.messages"; //$NON-NLS-1$
-
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+	private static final String BUNDLE_NAME = "org.eclipse.ve.internal.jfc.codegen.messages";//$NON-NLS-1$
 
 	private JFCCodegenMessages() {
+		// Do not instantiate
 	}
 
-	public static String getString(String key) {
-		// TODO Auto-generated method stub
-		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
-			return '!' + key + '!';
-		}
+	public static String AWTChooseBeanContributor_Label;
+	public static String SwingChooseBeanContributor_Label;
+
+	static {
+		NLS.initializeMessages(BUNDLE_NAME, JFCCodegenMessages.class);
 	}
 }
