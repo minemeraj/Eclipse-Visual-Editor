9027c3d06cc76300df732d75023b1dde6484fd93
dorme <dorme@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-07-04 00:18:44 +0000

    Added IInputChangeListener plus implemented support for it in IObjectEditor

---
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet/dataeditors/InputChangeAdapter.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2005 db4objects Inc.  http://www.db4o.com
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     db4objects - Initial API and implementation
+ */
+package org.eclipse.ve.sweet.dataeditors;
+
+/**
+ * InputChangeAdapter.
+ * 
+ * A class assisting to implement IInputChangeListener.
+ *
+ * @author djo
+ */
+public abstract class InputChangeAdapter implements IInputChangeListener {
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ve.sweet.dataeditors.IInputChangeListener#inputChanging(java.lang.Object, java.lang.Object)
+     */
+    public boolean inputChanging(Object oldInput, Object newInput) {
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ve.sweet.dataeditors.IInputChangeListener#inputChanged(java.lang.Object)
+     */
+    public abstract void inputChanged(Object newInput);
+
+}
