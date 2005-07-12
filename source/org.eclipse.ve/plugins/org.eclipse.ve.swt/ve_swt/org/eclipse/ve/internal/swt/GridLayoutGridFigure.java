e844c469e541dee47fc73bf1f4db3ee24aaf9e59
rkulp <rkulp@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-07-12 19:06:47 +0000

    [103535] Don't draw grid lines over spanned rows/cols.

--- source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/GridLayoutGridFigure.java
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/GridLayoutGridFigure.java
@@ -11,13 +11,13 @@
 package org.eclipse.ve.internal.swt;
 /*
  *  $RCSfile: GridLayoutGridFigure.java,v $
- *  $Revision: 1.10 $  $Date: 2005-07-11 20:03:52 $ 
+ *  $Revision: 1.11 $  $Date: 2005-07-12 19:06:47 $ 
  */
 
 import org.eclipse.draw2d.*;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
-import org.eclipse.swt.graphics.Color;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.swt.layout.GridLayout;
 
 /**
@@ -27,15 +27,16 @@ public class GridLayoutGridFigure extends Figure {
 	// number of pixels either side of a row/column that will trigger showing the row/column insertion figure.
 	public static final int ROW_COLUMN_SENSITIVITY = 5;
 	
-	Rectangle clientArea = new Rectangle();
-	Point mousePosition = null;
-	int[] columnWidths = null, rowHeights = null, columnPositions = null, rowPositions = null;
-	Point [] rowStartPositions = null, rowEndPositions = null;
-	Point [] columnStartPositions = null, columnEndPositions = null;
+	Rectangle clientArea;
+	Point mousePosition;
+	int[] columnPositions, rowPositions;
+	Point [] rowStartPositions, rowEndPositions;
+	Point [] columnStartPositions, columnEndPositions;
+	int [][] columnSegments, rowSegments;
 	
-	int marginWidth, marginHeight, verticalSpacing, horizontalSpacing, bonusWidth = 0, bonusHeight = 0;
+	int marginWidth, marginHeight, verticalSpacing, horizontalSpacing;
 
-public GridLayoutGridFigure (Rectangle bounds, int [][] layoutDimensions, Rectangle spacing, Rectangle clientArea) {
+public GridLayoutGridFigure (Rectangle bounds, int [][] layoutDimensions, EObject[][] cellContents, Rectangle spacing, Rectangle clientArea) {
 	super();
 	setBounds(bounds);
 	this.clientArea = clientArea;
@@ -53,95 +54,157 @@ public GridLayoutGridFigure (Rectangle bounds, int [][] layoutDimensions, Rectan
 		verticalSpacing = spacing.height;
 	}
 	if (layoutDimensions != null) {
-		columnWidths = layoutDimensions[0];
-		rowHeights = layoutDimensions[1];
-		calculateColumnDividers();
-		calculateRowDividers();
+		int[] columnWidths = layoutDimensions[0];
+		int [] rowHeights = layoutDimensions[1];
+		columnSegments = calculateColumnDividers(columnWidths, rowHeights, cellContents);
+		rowSegments = calculateRowDividers(columnWidths, rowHeights, cellContents);
 	}
 }
 
 protected void paintFigure(Graphics g){
-	Color orgColor = g.getForegroundColor();
-	
 	g.setForegroundColor(ColorConstants.gray);
 	g.setLineStyle(Graphics.LINE_DOT);
 	drawColumnDividers(g);
 	drawRowDividers(g);
-	
-	g.setForegroundColor(orgColor);
 }
 
 /**
  * Draw the column dividers based on the GridBagLayout's origin and the column widths.
- * Note: Column widths from GridBagLayout that equal zero indicate there are no components
+ * Return the columnSegments array.
+ * <p>
+ * <b>Note:</b> Column widths from GridBagLayout that equal zero indicate there are no components
  * in that specific column.
+ *
+ * @param columnWidths
+ * @param rowHeights
+ * @param cellContents
+ * @return columnSegements array. First dimension is for each column. Second dimension is the array of segments to draw
+ * for that column. For example, if there are no spaned columns, then the path for that column will be just one segment,
+ * top to bottom. The segments will be 2-tuples (startY, endY). It will only be the segments for drawing the non-spanned
+ * columns. Spanned columns aren't drawn.
+ * 
+ * @since 1.1.0
  */
-protected void calculateColumnDividers() {
-	if (rowHeights == null || columnWidths == null || clientArea == null) return;
+protected int[][] calculateColumnDividers(int[] columnWidths, int[] rowHeights, EObject[][] cellContents) {
+	if (rowHeights == null || columnWidths == null || clientArea == null) return null;
 	
-	int spacingTop = (int)Math.ceil((double)horizontalSpacing / 2);
-	int spacingBottom = (int)Math.floor((double)horizontalSpacing / 2);
+	int spacingLeft = (int)Math.ceil((double)horizontalSpacing / 2);
+	int spacingRight = (int)Math.floor((double)horizontalSpacing / 2);
+	int spacingTop = (int)Math.ceil((double)verticalSpacing / 2);
+	int spacingBottom = (int)Math.floor((double)verticalSpacing / 2);
 		
 	Rectangle r = getBounds();
 	int containerHeight = 0;
 	columnPositions = new int [columnWidths.length+1];
-	columnStartPositions = new Point [columnWidths.length+1];
-	columnEndPositions = new Point [columnWidths.length+1];
+	columnStartPositions = new Point [columnPositions.length];
+	columnEndPositions = new Point [columnPositions.length];
+	int[][] columnSegments = new int[columnPositions.length][];
 
-	if (bonusHeight != 0) {
-		containerHeight = clientArea.height;
-	} else {
-		for (int i = 0; i < rowHeights.length; i++) {
-			containerHeight += rowHeights[i];
-		}
-		// add height for the spacing
-		containerHeight += marginHeight * 2;
-		containerHeight += verticalSpacing * (rowHeights.length - 1);
+	for (int i = 0; i < rowHeights.length; i++) {
+		containerHeight += rowHeights[i];
 	}
+	// add height for the spacing
+	containerHeight += marginHeight * 2;
+	containerHeight += verticalSpacing * (rowHeights.length - 1);
 		
 	int xPos = r.x+ clientArea.x;
 	int yMin = r.y + clientArea.y;
 	int yMax = r.y + clientArea.y + containerHeight;
+	int xMax = xPos+clientArea.width-1;	// This is the far right side, less one so that it draws within the box (otherwise it would be outside the box).
 	
 	// draw the first divider
 	columnPositions[0] = xPos;
 	columnStartPositions[0] = new Point(xPos, yMin);
 	columnEndPositions[0] = new Point(xPos, yMax);
+	columnSegments[0] = new int[] {yMin, yMax};	// The left border will always be one segemnt.
 	
 	// move up by the initial margin width
 	xPos += marginWidth;
 	
+	int[] colSegs = new int[2+rowHeights.length*2];	// Each entry is a y position. They are in 2-tuples (start,stop) for a segment. For a col, it is max of one segment(or 2 points) + 2 for start of next.	
 	// draw the dividers in between and at the end
-	for (int i = 0; i < columnWidths.length; i++) {
-		xPos += columnWidths[i];
+	for (int i = 1; i < columnPositions.length; i++) {
+		xPos += columnWidths[i-1];
 		
 		// Place the position in the middle of the the horizontal spacing gap
-		if (i != columnWidths.length - 1) {
-			xPos += spacingTop;
+		if (i < columnWidths.length) {
+			xPos += spacingLeft;
 		} else {
 			// or after the end margin, if this is the last line
 			xPos += marginWidth;
 		}
 		
-		if (xPos >= r.x+r.width)
-			xPos = r.x+r.width-1;
+		xPos = Math.min(xPos, xMax);
 		Point startPoint = new Point(xPos, yMin );
 		Point endPoint = new Point(xPos, yMax );
-		columnPositions[i+1] = xPos;
-		columnStartPositions[i+1] = startPoint;
-		columnEndPositions[i+1] = endPoint;
+		columnPositions[i] = xPos;
+		columnStartPositions[i] = startPoint;
+		columnEndPositions[i] = endPoint;
+		
+		// Now calculate the column segments.
+		if (i < columnWidths.length) {
+			EObject[] leftColumn = cellContents[i-1];
+			EObject[] rightColumn = cellContents[i];
+			int yPos = yMin;
+			int lastRow = leftColumn.length-1;
+			int colSegsNdx = 0;	// This will always point to the start index of the next segment.
+			colSegs[colSegsNdx] = yMin;	// Will always start at yMin.
+			// Walk each row, and compare left and right column to see if spanned.
+			for (int j = 0; j < leftColumn.length; j++) {
+				int trueRowHeight = rowHeights[j];
+				if (j == 0)
+					trueRowHeight+=marginHeight;
+				else
+					trueRowHeight+=spacingTop;
+				if (j != lastRow)
+					trueRowHeight+=spacingBottom;
+				else
+					trueRowHeight+=marginHeight;
+				EObject leftObject, rightObject;
+				if ((leftObject = leftColumn[j]) != GridLayoutPolicyHelper.EMPTY && (rightObject = rightColumn[j]) != GridLayoutPolicyHelper.EMPTY && leftObject == rightObject) {
+					// We are spanning, so skip it and move start of segment to next cell.
+					if (colSegsNdx != 0) {
+						// Need to close off previous one (if not first)
+						colSegs[++colSegsNdx] = yPos;
+						colSegsNdx++;	// Move to start of next.
+					}
+					yPos+=trueRowHeight;
+					colSegs[colSegsNdx] = yPos+1;	// Start of next seg.
+				} else {
+					// We are not spanning, continue line through it.
+					yPos+=trueRowHeight;
+				}
+			}
+			if (colSegs[colSegsNdx] < yPos) {
+				// We have to something to draw for last segment.
+				colSegs[++colSegsNdx] = yMax;
+			} else
+				colSegsNdx--;	// We had a segment start that is the same as the last stop, so get rid of it.
+			columnSegments[i] = new int[++colSegsNdx];
+			System.arraycopy(colSegs, 0, columnSegments[i], 0, colSegsNdx);
+		} else {
+			columnSegments[i] = new int[] {yMin, yMax};	// The right border will always be one segemnt.
+		}
 		// add the remainder of the spacing
-		xPos += spacingBottom;
+		xPos += spacingRight;
 	}
+	
+	return columnSegments;
 }
 /**
  * Draw the column dividers based on the GridBagLayout's origin and the column widths.
  */
 protected void drawColumnDividers(Graphics g) {
-	if (columnStartPositions == null && columnEndPositions == null)
+	if (columnSegments == null || columnStartPositions == null)
 		return;
-	for (int i = 0; i < columnStartPositions.length; i++) 
-		g.drawLine(columnStartPositions[i], columnEndPositions[i]);
+	for (int i = 0; i < columnSegments.length; i++) {
+		int[] colSegs = columnSegments[i];
+		int xPos = columnStartPositions[i].x;
+		int j=-1;
+		while(++j < colSegs.length) {
+			g.drawLine(xPos, colSegs[j], xPos, colSegs[++j]);
+		}
+	}
 }
 
 /**
@@ -149,73 +212,127 @@ protected void drawColumnDividers(Graphics g) {
  * Note: Row heights from GridBagLayout that equal zero indicate there are no components
  * in that specific row.
  */
-protected void calculateRowDividers() {
-	if (rowHeights == null || columnWidths == null || clientArea == null) return;
+protected int[][] calculateRowDividers(int[] columnWidths, int[] rowHeights, EObject[][] cellContents) {
+	if (columnWidths == null || rowHeights == null || clientArea == null) return null;
 	
-	int spacingLeft = (int)Math.ceil((double)verticalSpacing / 2);
-	int spacingRight = (int)Math.floor((double)verticalSpacing / 2);
+	int spacingLeft = (int)Math.ceil((double)horizontalSpacing / 2);
+	int spacingRight = (int)Math.floor((double)horizontalSpacing / 2);
+	int spacingTop = (int)Math.ceil((double)verticalSpacing / 2);
+	int spacingBottom = (int)Math.floor((double)verticalSpacing / 2);
 	
 	Rectangle r = getBounds();
 	int containerWidth = 0;
 	rowPositions = new int [rowHeights.length+1];
-	rowStartPositions = new Point [rowHeights.length+1];
-	rowEndPositions = new Point [rowHeights.length+1];
+	rowStartPositions = new Point [rowPositions.length];
+	rowEndPositions = new Point [rowPositions.length];
+	int[][] rowSegments = new int[rowPositions.length][];
 
-	if (bonusWidth != 0) {
-		containerWidth = clientArea.width;
-	} else {
-		for (int i = 0; i < columnWidths.length; i++) {
-			containerWidth += columnWidths[i];
-		}
-		// add width for the spacing
-		containerWidth += marginWidth * 2;
-		containerWidth += horizontalSpacing * ( columnWidths.length - 1);
+	for (int i = 0; i < columnWidths.length; i++) {
+		containerWidth += columnWidths[i];
 	}
+	// add width for the spacing
+	containerWidth += marginWidth * 2;
+	containerWidth += horizontalSpacing * ( columnWidths.length - 1);
 	
 	int yPos = r.y+ clientArea.y;
 	int xMin = r.x + clientArea.x;
 	int xMax = r.x + clientArea.x + containerWidth;
+	int yMax = yPos+clientArea.height-1;	// This is the bottom side, less one so that it draws within the box (otherwise it would be outside the box).
 	
 	// draw the first divider
 	rowPositions[0] = yPos;
 	rowStartPositions[0] = new Point(xMin, yPos);
 	rowEndPositions[0] = new Point(xMax, yPos);
+	rowSegments[0] = new int[] {xMin, xMax};	// The top border will always be one segemnt.
 	
 	// move up the initial margin height
 	yPos += marginHeight;
 	
+	int[] rowSegs = new int[2+columnWidths.length*2];	// Each entry is an x position. They are in 2-tuples (start,stop) for a segment. For a row, it is max of one segment(or 2 points) + 2 for start of next.
 	// draw the dividers in between and at the end
-	for (int i = 0; i < rowHeights.length; i++) {
-		yPos += rowHeights[i];
+	for (int i = 1; i < rowPositions.length; i++) {
+		yPos += rowHeights[i-1];
 	
-		if (yPos >= r.y+r.height) 
-			yPos = r.y+r.height-1;
-
 		// Place the position in the middle of the the vertical spacing gap
-		if (i != rowHeights.length - 1) {
-			yPos += spacingLeft;
+		if (i < rowHeights.length) {
+			yPos += spacingTop;
 		} else {
 			// or after the end margin, if this is the last line
 			yPos += marginHeight;
 		}
 		
+		yPos = Math.min(yPos, yMax);
 		Point startPoint = new Point(xMin, yPos );
 		Point endPoint = new Point(xMax, yPos );
-		rowPositions[i+1] = yPos;
-		rowStartPositions[i+1] = startPoint;
-		rowEndPositions[i+1] = endPoint;
+		rowPositions[i] = yPos;
+		rowStartPositions[i] = startPoint;
+		rowEndPositions[i] = endPoint;
+		
+		// Now calculate the row segments.
+		if (i < rowHeights.length) {
+			int upperRow = i-1;
+			int lowerRow = i;
+			int xPos = xMin;
+			int lastCol = cellContents.length-1;
+			int rowSegsNdx = 0;	// This will always point to the start index of the next segment.
+			rowSegs[rowSegsNdx] = xMin;	// Will always start at xMin.
+			// Walk each column comparing upper row and lower to see if spanned.
+			for (int j = 0; j < cellContents.length; j++) {
+				int trueColWidth = columnWidths[j];
+				if (j == 0)
+					trueColWidth+=marginWidth;
+				else
+					trueColWidth+=spacingLeft;
+				if (j != lastCol)
+					trueColWidth+=spacingRight;
+				else
+					trueColWidth+=marginWidth;
+				EObject upperObject, lowerObject;
+				if ((upperObject = cellContents[j][upperRow]) != GridLayoutPolicyHelper.EMPTY && (lowerObject = cellContents[j][lowerRow]) != GridLayoutPolicyHelper.EMPTY && upperObject == lowerObject) {
+					// We are spanning, so skip it and move start of segment to next cell.
+					if (rowSegsNdx != 0) {
+						// Need to close off previous one (if not first)
+						rowSegs[++rowSegsNdx] = xPos;
+						rowSegsNdx++;	// Move to start of next.
+					}
+					xPos+=trueColWidth;
+					rowSegs[rowSegsNdx] = xPos+1;	// Start of next seg.
+				} else {
+					// We are not spanning, continue line through it.
+					xPos+=trueColWidth;
+				}
+			}
+			if (rowSegs[rowSegsNdx] < xPos) {
+				// We have to something to draw for last segment.
+				rowSegs[++rowSegsNdx] = xMax;
+			} else
+				rowSegsNdx--;	// We had a segment start that is the same as the last stop, so get rid of it.
+			rowSegments[i] = new int[++rowSegsNdx];
+			System.arraycopy(rowSegs, 0, rowSegments[i], 0, rowSegsNdx);
+		} else {
+			rowSegments[i] = new int[] {xMin, xMax};	// The bottom border will always be one segemnt.
+		}
+		
 		// add the remainder of the spacing		
-		yPos += spacingRight;
+		yPos += spacingBottom;
 	}
+	
+	return rowSegments;
 }
 /**
- * Draw the row dividers based on the on the GridBagLayout's origin and the row heights.
+ * Draw the row dividers based on the GridBagLayout's origin and the row widths.
  */
 protected void drawRowDividers(Graphics g) {
-	if (rowStartPositions == null && rowEndPositions == null)
+	if (rowSegments == null || rowStartPositions == null)
 		return;
-	for (int i = 0; i < rowStartPositions.length; i++) 
-		g.drawLine(rowStartPositions[i], rowEndPositions[i]);
+	for (int i = 0; i < rowSegments.length; i++) {
+		int[] rowSegs = rowSegments[i];
+		int yPos = rowStartPositions[i].y;
+		int j=-1;
+		while(++j < rowSegs.length) {
+			g.drawLine(rowSegs[j], yPos, rowSegs[++j], yPos);
+		}
+	}
 }
 
 /**
@@ -284,12 +401,6 @@ public Point getCellLocation(int x, int y, boolean includeEmptyColumns, boolean 
 		gridy = -1;
 		
 	return new Point(gridx,gridy);
-}
-
-public int getRowHeight(int row) {
-	if (rowHeights == null || row < 0 || row >= rowHeights.length)
-		return -1;
-	else return rowHeights[row];
 }
 
 /**
@@ -450,13 +561,5 @@ public int getNearestColumn(int x) {
 		}
 	} 
 	return column;
-}
-public void setLayoutDimensions(int[][] dim) {
-	if (dim == null) return;
-	columnWidths = dim[0];
-	rowHeights = dim[1];
-	calculateColumnDividers();
-	calculateRowDividers();
-	invalidate();
 }
 }
