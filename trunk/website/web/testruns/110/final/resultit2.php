4ddfe8fe02ef64054b1c5af6fc4a8354dab1ef26
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-07-13 15:39:18 +0000

    Updated to post mail to ve-dev instead of walkerp

--- trunk/website/web/testruns/110/final/resultit2.php
+++ trunk/website/web/testruns/110/final/resultit2.php
@@ -10,19 +10,19 @@ header("Location: http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."
     	$datafile = "/home/data/httpd/writable/vep/testruns/110/final/tests-ve1.1.txt";
     	$newdatafile = "/home/data/httpd/writable/vep/testruns/110/final/tests-ve1.1.txt.new";
        if (!($f=fopen($datafile,"r")))
-                  exit("Unable to open file.");
+                  exit("Unable to open original file.");
        if (!flock($f, LOCK_EX)) 
                   exit ("Site is busy") ;
        if (!($f2=fopen($newdatafile,"w+"))) 
-                  exit("Unable to open file.");  
+                  exit("Unable to open temp file.");  
        while ($testinfo = fscanf ($f, "%s\t%s\t%s\t%s\t%[a-zA-Z0-9,. \\'';;~~!!@@##$$%%&&**(())--==++__]\n")) {
               list ($tst, $url, $tester, $status, $description) = $testinfo;
               if (($tst == $_POST["Test"]) && ($_POST["Result"]!="")) {
                   $status = $_POST["Result"];
                   $description=$_POST["Comments"];
                   // ve-dev@eclipse.org 
-//                  mail("ve-dev@eclipse.org, ".$_POST["Email"], $_POST["Test"].": ".$_POST["Result"].".", "VisualEditor, 1.1 final TestPass\n\tTestcase " . $_POST["Test"] . " status=".$_POST["Result"]." by ". $_POST["Email"]."\nComments:\n".$description.".", "From: VEproject@{$_SERVER['SERVER_NAME']}");                  
-                  mail("walkerp@us.ibm.com, ".$_POST["Email"], $_POST["Test"].": ".$_POST["Result"].".", "VisualEditor, 1.1 final TestPass\n\tTestcase " . $_POST["Test"] . " status=".$_POST["Result"]." by ". $_POST["Email"]."\nComments:\n".$description.".", "From: gmendel@us.ibm.com");                  
+                  mail("ve-dev@eclipse.org, ".$_POST["Email"], $_POST["Test"].": ".$_POST["Result"].".", "VisualEditor, 1.1 final TestPass\n\tTestcase " . $_POST["Test"] . " status=".$_POST["Result"]." by ". $_POST["Email"]."\nComments:\n".$description.".", "From: VEproject@{$_SERVER['SERVER_NAME']}");                  
+//                  mail("walkerp@us.ibm.com, ".$_POST["Email"], $_POST["Test"].": ".$_POST["Result"].".", "VisualEditor, 1.1 final TestPass\n\tTestcase " . $_POST["Test"] . " status=".$_POST["Result"]." by ". $_POST["Email"]."\nComments:\n".$description.".", "From: gmendel@us.ibm.com");                  
               }
               $out=sprintf("%s\t%s\t%s\t%s\t%s\n", $tst, $url, $tester, $status, $description);
               fwrite($f2,$out);
