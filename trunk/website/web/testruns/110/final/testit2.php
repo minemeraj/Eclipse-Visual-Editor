54fcae5062657488c424157fe076e2cdddaac794
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-07-13 22:29:31 +0000

    Backed out sending from VE server... need actual email address.. put Gili's back in

--- trunk/website/web/testruns/110/final/testit2.php
+++ trunk/website/web/testruns/110/final/testit2.php
@@ -25,7 +25,7 @@ header("Location: http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."
                   $tester = $_POST["Email"];                  
                   // ve-dev@eclipse.org 
 //                  mail("walkerp@us.ibm.com, " . $_POST["Email"], $_POST["Test"] . " assigned to " . $_POST["Email"], "VisualEditor, 1.1 Final TestPass\n\tTestcase " . $_POST["Test"] . " was assigned to ". $_POST["Email"].".", "From: VEproject@{$_SERVER['SERVER_NAME']}");                  
-                  mail("ve-dev@eclipse.org, " . $_POST["Email"], $_POST["Test"] . " assigned to " . $_POST["Email"], "VisualEditor, 1.1 Final TestPass\n\tTestcase " . $_POST["Test"] . " was assigned to ". $_POST["Email"].".", "From: VEproject@{$_SERVER['SERVER_NAME']}");                  
+                  mail("ve-dev@eclipse.org, " . $_POST["Email"], $_POST["Test"] . " assigned to " . $_POST["Email"], "VisualEditor, 1.1 Final TestPass\n\tTestcase " . $_POST["Test"] . " was assigned to ". $_POST["Email"].".", "From: gmendel@us.ibm.com");                  
               }
               $out=sprintf("%s\t%s\t%s\t%s\t%s\n", $tst, $url, $tester, $status, $description);
               fwrite($f2,$out);
