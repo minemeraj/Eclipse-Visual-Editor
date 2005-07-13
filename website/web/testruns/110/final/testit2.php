dd81057c01ec994e20706dc011574af847a06295
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-07-13 15:29:04 +0000

    Show PHP error message

--- website/web/testruns/110/final/testit2.php
+++ website/web/testruns/110/final/testit2.php
@@ -16,7 +16,7 @@
        if (!flock($f, LOCK_EX)) 
                   exit ("Site is busy") ;
        if (!($f2=fopen($newdatafile,"w+"))) 
-                  exit("Unable to open temp file.");  
+                  exit("Unable to open temp file. '$php_errormsg'");  
        while ($testinfo = fscanf ($f, "%s\t%s\t%s\t%s\t%[a-zA-Z0-9,. \\'';;~~!!@@##$$%%&&**(())--==++__]\n")) {
               list ($tst, $url, $tester, $status, $description) = $testinfo;
               // No spaces allowed in an Email ID
