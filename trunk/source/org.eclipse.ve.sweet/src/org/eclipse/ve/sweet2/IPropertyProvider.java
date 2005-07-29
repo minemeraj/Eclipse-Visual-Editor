36595c5653afd81d47a72522b26149d399b55e25
gmendel <gmendel@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-07-29 20:47:30 +0000

    Start on jFacezing

---
+++ trunk/source/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet2/IPropertyProvider.java
@@ -0,0 +1,27 @@
+
+package org.eclipse.ve.sweet2;
+import org.eclipse.jface.viewers.IContentProvider;
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*
+ *  $RCSfile: IPropertyProvider.java,v $
+ *  $Revision: 1.1 $  $Date: 2005-07-29 20:47:30 $ 
+ */
+
+/**
+ * 
+ * @since 1.0.0
+ */
+public interface IPropertyProvider extends IContentProvider {
+
+	public Object getValue();
+	public void setValue(Object value);
+}
