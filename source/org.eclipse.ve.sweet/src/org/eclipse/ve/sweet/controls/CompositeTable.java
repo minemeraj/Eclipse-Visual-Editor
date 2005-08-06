2545df45c465e3b7491464856f61cd27e15a9096
dorme <dorme@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-08-06 11:51:24 +0000

    Refactored; added JavaDocs

--- source/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet/controls/CompositeTable.java
+++ source/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet/controls/CompositeTable.java
@@ -27,8 +27,24 @@ import org.eclipse.swt.widgets.Layout;
 
 
 /**
- * Class CompositeTable.  A virtual table control that works by stacking copies of a
- * programmer-supplied Composite below each other.<p>
+ * Class CompositeTable.  A virtual table control that works by laying out multiple
+ * copies of a programmer-supplied Composite below each other.<p>
+ * 
+ * Synopsis:<p>
+ * 
+ * In order to edit anything, one must:<p>
+ * 
+ * <ul>
+ * <li>Set up a prototype row (and optionally header) object as specified in the CompositeTable
+ * class documentation.
+ * <li>Set the RunTime property to "true".
+ * <li>Add a RowContentProvider that knows how to put data into a row's controls.
+ * <li>Add a RowListener to validate and save changed data.
+ * <li>Set the NumRowsInCollection property to the number of rows in the underlying data
+ * structure.
+ * </ul>
+ *
+ * Detailed description:<p>
  * 
  * This control is designed to work inside of the Eclipse Visual Editor.  To use it,
  * drop one on the design surface.  (Even though it extends Canvas, it does not make
@@ -337,21 +353,29 @@ public class CompositeTable extends Canvas {
 		}
 	}
 
-	public boolean isDrawingLines() {
-		return drawingLines;
-	}
-
-	public void setDrawingLines(boolean drawingLines) {
-		this.drawingLines = drawingLines;
-		if (contentPane != null) {
-			contentPane.setDrawingLines(drawingLines);
-		}
-	}
-
+	/**
+	 * Method getWeights.  Returns an array representing the percentage of the total width
+	 * each column is allocated or null if no weights have been specified.  This property is
+	 * ignored if the programmer has set a layout manager on the header and/or the row
+	 * prototype objects.
+	 * 
+	 * @return the current weights array or null if no weights have been specified.
+	 */
 	public int[] getWeights() {
 		return weights;
 	}
 
+	/**
+	 * Method setWeights.  Specifies the percentage of the total width that will be allocated
+	 * to each column.  This property is ignored if the programmer has set a layout manager 
+	 * on the header and/or the row prototype objects.<p>
+	 * 
+	 * The number of elements in the array must match the number of columns and the sum of
+	 * all elements must equal 100.  If either of these constraints is not true, this property
+	 * will be ignored and all columns will be created equal in width.
+	 * 
+	 * @param weights the weights to use if the CompositeTable is automatically laying out controls.
+	 */
 	public void setWeights(int[] weights) {
 		this.weights = weights;
 		if (contentPane != null) {
@@ -359,10 +383,30 @@ public class CompositeTable extends Canvas {
 		}
 	}
 
+	/**
+	 * Method getMaxRowsVisible.  Returns the maximum number of rows that will be permitted 
+	 * in the table at once.  For example, setting this property to 1 will have the effect of 
+	 * creating a single editing area with a scroll bar on the right allowing the user to scroll
+	 * through all rows using either the mouse or the PgUp/PgDn keys.  The default value is
+	 * Integer.MAX_VALUE.
+	 * 
+	 * @return the maximum number of rows that are permitted to be visible at one time, regardless
+	 * of the control's size.
+	 */
 	public int getMaxRowsVisible() {
 		return maxRowsVisible;
 	}
 
+	/**
+	 * Method setMaxRowsVisible.  Sets the maximum number of rows that will be permitted 
+	 * in the table at once.  For example, setting this property to 1 will have the effect of 
+	 * creating a single editing area with a scroll bar on the right allowing the user to scroll
+	 * through all rows using either the mouse or the PgUp/PgDn keys.  The default value is
+	 * Integer.MAX_VALUE.
+	 * 
+	 * @param maxRowsVisible the maximum number of rows that are permitted to be visible at one time, regardless
+	 * of the control's size.
+	 */
 	public void setMaxRowsVisible(int maxRowsVisible) {
 		this.maxRowsVisible = maxRowsVisible;
 		if (contentPane != null) {
@@ -370,6 +414,13 @@ public class CompositeTable extends Canvas {
 		}
 	}
 	
+	/**
+	 * Method getNumRowsVisible.  Returns the actual number of rows that are currently visible.
+	 * Normally CompositeTable displays as many rows as will fit vertically given the control's
+	 * size.  This value can be clamped to a maximum using the MaxRowsVisible property.
+	 * 
+	 * @return the actual number of rows that are currently visible.
+	 */
 	public int getNumRowsVisible() {
 		if (contentPane != null)
 			return contentPane.getNumRowsVisible();
@@ -377,10 +428,22 @@ public class CompositeTable extends Canvas {
 			return -1;
 	}
 
+	/**
+	 * Method getNumRowsInCollection.  Returns the number of rows in the data structure that is
+	 * being edited.
+	 * 
+	 * @return the number of rows in the underlying data structure.
+	 */
 	public int getNumRowsInCollection() {
 		return numRowsInCollection;
 	}
 
+	/**
+	 * Method setNumRowsInCollection.  Sets the number of rows in the data structure that is
+	 * being edited.
+	 * 
+	 * @param numRowsInCollection the number of rows represented by the underlying data structure.
+	 */
 	public void setNumRowsInCollection(int numRowsInCollection) {
 		this.numRowsInCollection = numRowsInCollection;
 		if (contentPane != null) {
@@ -388,10 +451,24 @@ public class CompositeTable extends Canvas {
 		}
 	}
 
+	/**
+	 * Method getTopRow.  Return the number of the line that is being displayed in the top row
+	 * of the CompositeTable editor (0-based).
+	 * 
+	 * @return the number of the top line.
+	 */
 	public int getTopRow() {
 		return topRow;
 	}
 
+	/**
+	 * Method setTopRow. Set the number of the line that is being displayed in the top row
+	 * of the CompositeTable editor (0-based).  If the new top row is not equal to the current
+	 * top row, the table will automatically be scrolled to the new position.  This number must
+	 * be greater than 0 and less than NumRowsInCollection.
+	 * 
+	 * @param topRow the line number of the new top row.
+	 */
 	public void setTopRow(int topRow) {
 		this.topRow = topRow;
 		if (contentPane != null) {
@@ -399,6 +476,12 @@ public class CompositeTable extends Canvas {
 		}
 	}
 
+	/**
+	 * Method getCurrentColumn.  Returns the column number of the currently-focused column 
+	 * (0-based).
+	 * 
+	 * @return the column number of the currently-focused column.
+	 */
 	public int getCurrentColumn() {
 		if (contentPane == null) {
 			return -1;
@@ -406,10 +489,23 @@ public class CompositeTable extends Canvas {
 		return getSelection().x;
 	}
 
+	/**
+	 * Method setCurrentColumn.  Sets the column number of the currently-focused column 
+	 * (0-based).
+	 * 
+	 * @param column The new column to focus.
+	 */
 	public void setCurrentColumn(int column) {
 		setSelection(column, getCurrentRow());
 	}
 
+	/**
+	 * Method getCurrentRow.  Returns the current row number as an offset from the top of the
+	 * table window.  In order to get the current row in the underlying data structure, compute
+	 * getTopRow() + getCurrentRow().
+	 * 
+	 * @return the current row number as an offset from the top of the table window.
+	 */
 	public int getCurrentRow() {
 		if (contentPane == null) {
 			return -1;
@@ -417,10 +513,25 @@ public class CompositeTable extends Canvas {
 		return getSelection().y;
 	}
 
+	/**
+	 * Method setCurrentRow.  Sets the current row number as an offset from the top of the
+	 * table window.  In order to get the current row in the underlying data structure, compute
+	 * getTopRow() + getCurrentRow().
+	 * 
+	 * @param row  the current row number as an offset from the top of the table window.
+	 */
 	public void setCurrentRow(int row) {
 		setSelection(getCurrentColumn(), row);
 	}
 	
+	/**
+	 * Method getSelection.  Returns the currently-selected (column, row) pair where the row 
+	 * specifies the offset from the top of the table window.  In order to get the current 
+	 * row in the underlying data structure, use getSelection().y + getCurrentRow().
+	 * 
+	 * @return  the currently-selected (column, row) pair where the row specifies the offset 
+	 * from the top of the table window.
+	 */
 	public Point getSelection() {
 		if (contentPane == null) {
 			return null;
@@ -428,10 +539,25 @@ public class CompositeTable extends Canvas {
 		return contentPane.getSelection();
 	}
 	
+	/**
+	 * Method setSelection.  Sets the currently-selected (column, row) pair where the row 
+	 * specifies the offset from the top of the table window.  In order to get the current 
+	 * row in the underlying data structure, use getSelection().y + getCurrentRow().
+	 * 
+	 * @param selection the (column, row) to select
+	 */
 	public void setSelection(Point selection) {
 		setSelection(selection.x, selection.y);
 	}
 
+	/**
+	 * Method setSelection.  Sets the currently-selected (column, row) pair where the row 
+	 * specifies the offset from the top of the table window.  In order to get the current 
+	 * row in the underlying data structure, use getSelection().y + getCurrentRow().
+	 * 
+	 * @param column the column to select
+	 * @param row the row to select as an offset from the top of the window
+	 */
 	public void setSelection(int column, int row) {
 		if (contentPane == null) {
 			return;
@@ -439,45 +565,87 @@ public class CompositeTable extends Canvas {
 		contentPane.setSelection(column, row);
 	}
 	
-	public Control[] getRowControls() {
-		if (contentPane == null) {
-			return null;
-		}
-		return contentPane.getRowControls();
-	}
-
+	/** (non-API)
+	 * Method getHeaderConstructor.  Returns the Constructor object used internally to
+	 * construct the table's header or null if there is none.
+	 * 
+	 * @return the header's constructor.
+	 */
 	public Constructor getHeaderConstructor() {
 		return headerConstructor;
 	}
 
+	/** (non-API)
+	 * Method getRowConstructor.  Returns the Constructor object used internally to 
+	 * construct each row object.
+	 * 
+	 * @return the rows' constructor
+	 */
 	public Constructor getRowConstructor() {
 		return rowConstructor;
 	}
 
+	/** (non-API)
+	 * Method getHeaderControl.  Returns the prototype header control.
+	 * 
+	 * @return the prototype header control.
+	 */
 	public Control getHeaderControl() {
 		return headerControl;
 	}
 
+	/** (non-API)
+	 * Method getRowControl.  Returns the prototype row control.
+	 * 
+	 * @return the prototype row control.
+	 */
 	public Control getRowControl() {
 		return rowControl;
 	}
 
-	LinkedList refreshListeners = new LinkedList();
+	LinkedList contentProviders = new LinkedList();
 	
-	public void addRefreshContentProvider(IRowContentProvider listener) {
-		refreshListeners.add(listener);
+	/**
+	 * Method addRowContentProvider.  Adds the specified content provider to the list of
+	 * content providers that will be called when a row needs to be filled with data.
+	 * Most of the time it only makes sense to add a single one.
+	 * 
+	 * @param contentProvider The content provider to add.
+	 */
+	public void addRowContentProvider(IRowContentProvider contentProvider) {
+		contentProviders.add(contentProvider);
 	}
 
-	public void removeRefreshContentProvider(IRowContentProvider listener) {
-		refreshListeners.remove(listener);
+	/**
+	 * Method removeRowContentProvider.  Removes the specified content provider from the list of
+	 * content providers that will be called when a row needs to be filled with data.
+	 * 
+	 * @param contentProvider The content provider to remove.
+	 */
+	public void removeRowContentProvider(IRowContentProvider contentProvider) {
+		contentProviders.remove(contentProvider);
 	}
 	
 	LinkedList rowListeners = new LinkedList();
 	
+	/**
+	 * Method addRowListener.  Adds the specified listener to the set of listeners that 
+	 * will be notified when the user wishes to leave a row and when the user has already
+	 * left a row.  If any listener vetos leaving a row, the focus remains in the row.
+	 * 
+	 * @param listener The listener to add.
+	 */
 	public void addRowListener(IRowListener listener) {
 		rowListeners.add(listener);
 	}
 	
+	/**
+	 * Method removeRowListener.  Removes the specified listener from the set of listeners that 
+	 * will be notified when the user wishes to leave a row and when the user has already
+	 * left a row.  If any listener vetos leaving a row, the focus remains in the row.
+	 * 
+	 * @param listener The listener to remove.
+	 */
 	public void removeRowListener(IRowListener listener) {
 		rowListeners.remove(listener);
 	}
