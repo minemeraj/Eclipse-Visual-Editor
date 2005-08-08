39aafd01018825c0587ee8cd772710700b3c62d4
dorme <dorme@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-08-08 15:27:52 +0000

    Values default to empty strings

--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet/controls/test/Person.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet/controls/test/Person.java
@@ -2,10 +2,10 @@ package org.eclipse.ve.sweet.controls.test;
 
 public class Person {
 	
-	public String name;
-	public String address;
-	public String city;
-	public String state;
+	public String name = "";
+	public String address = "";
+	public String city = "";
+	public String state = "";
 	
 	public Person(String name, String address, String city, String state) {
 		this.address = address;
@@ -13,4 +13,6 @@ public class Person {
 		this.name = name;
 		this.state = state;
 	}
+	
+	public Person() {}
 }
