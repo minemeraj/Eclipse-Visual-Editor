360f2a029e5c3728862566518de1f82a5e599f1b
rkulp <rkulp@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-08-10 20:14:14 +0000

    [56807] Stop allowing java keywords as field names.

--- source/org.eclipse.ve.java.core/codegen/org/eclipse/ve/internal/java/codegen/editorpart/FieldNameValidator.java
+++ source/org.eclipse.ve.java.core/codegen/org/eclipse/ve/internal/java/codegen/editorpart/FieldNameValidator.java
@@ -10,10 +10,12 @@
  *******************************************************************************/
 /*
  *  $RCSfile: FieldNameValidator.java,v $
- *  $Revision: 1.5 $  $Date: 2005-06-21 22:12:53 $ 
+ *  $Revision: 1.6 $  $Date: 2005-08-10 20:14:14 $ 
  */
 package org.eclipse.ve.internal.java.codegen.editorpart;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 
 import org.eclipse.emf.ecore.EObject;
@@ -72,6 +74,61 @@ public class FieldNameValidator implements ICellEditorValidator, ISourced {
 		return null;
 	}
 	
+	private static final Set KEYWORDS = new HashSet(20);
+	static {
+		// This is the list of keywords as of Java 5.0. 
+		KEYWORDS.add("abstract");
+		KEYWORDS.add("assert");
+		KEYWORDS.add("byte");
+		KEYWORDS.add("boolean");
+		KEYWORDS.add("break");
+		KEYWORDS.add("case");
+		KEYWORDS.add("char");
+		KEYWORDS.add("catch");
+		KEYWORDS.add("class");
+		KEYWORDS.add("continue");
+		KEYWORDS.add("do");
+		KEYWORDS.add("double");
+		KEYWORDS.add("default");
+		KEYWORDS.add("else");
+		KEYWORDS.add("extends");
+		KEYWORDS.add("for");
+		KEYWORDS.add("final");
+		KEYWORDS.add("float");
+		KEYWORDS.add("false");
+		KEYWORDS.add("finally");
+		KEYWORDS.add("if");
+		KEYWORDS.add("int");
+		KEYWORDS.add("import");
+		KEYWORDS.add("interface");
+		KEYWORDS.add("implements");
+		KEYWORDS.add("instanceof");
+		KEYWORDS.add("long");
+		KEYWORDS.add("new");
+		KEYWORDS.add("null");
+		KEYWORDS.add("native");
+		KEYWORDS.add("public");
+		KEYWORDS.add("package");
+		KEYWORDS.add("private");
+		KEYWORDS.add("protected");
+		KEYWORDS.add("return");
+		KEYWORDS.add("short");
+		KEYWORDS.add("super");
+		KEYWORDS.add("static");
+		KEYWORDS.add("switch");
+		KEYWORDS.add("strictfp");
+		KEYWORDS.add("synchronized");
+		KEYWORDS.add("try");
+		KEYWORDS.add("this");
+		KEYWORDS.add("true");
+		KEYWORDS.add("throw");
+		KEYWORDS.add("throws");
+		KEYWORDS.add("transient");
+		KEYWORDS.add("void");
+		KEYWORDS.add("volatile");
+		KEYWORDS.add("while");
+	}
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.jface.viewers.ICellEditorValidator#isValid(java.lang.Object)
 	 */
@@ -85,6 +142,8 @@ public class FieldNameValidator implements ICellEditorValidator, ISourced {
 			if(name.length()>0 && !Character.isJavaIdentifierStart(name.charAt(0)))
 				return CodegenEditorPartMessages.FieldNameValidator_InvalidVariableName_INFO_; 
 			if(name.length()>1){
+				if (KEYWORDS.contains(name))
+					return CodegenEditorPartMessages.FieldNameValidator_InvalidVariableName_INFO_;
 				for (int cc = 1; cc < name.length(); cc++) {
 					if(!Character.isJavaIdentifierPart(name.charAt(cc)))
 						return CodegenEditorPartMessages.FieldNameValidator_InvalidVariableName_INFO_; 
