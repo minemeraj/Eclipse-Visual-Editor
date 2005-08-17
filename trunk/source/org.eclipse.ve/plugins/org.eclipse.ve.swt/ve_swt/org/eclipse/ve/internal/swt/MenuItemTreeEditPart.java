5634ab0b6fd2705c52619f13b2a2b115ad31fc32
rkulp <rkulp@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-08-17 18:39:48 +0000

    [106694] Work to get swt Menus tolerated.

--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/MenuItemTreeEditPart.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/ve_swt/org/eclipse/ve/internal/swt/MenuItemTreeEditPart.java
@@ -12,12 +12,14 @@
  *  Created Aug 15, 2005 by Gili Mendel
  * 
  *  $RCSfile: MenuItemTreeEditPart.java,v $
- *  $Revision: 1.1 $  $Date: 2005-08-17 12:30:36 $ 
+ *  $Revision: 1.2 $  $Date: 2005-08-17 18:39:48 $ 
  */
 package org.eclipse.ve.internal.swt;
 
 import java.util.*;
 
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -25,12 +27,18 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jem.internal.instantiation.base.IJavaObjectInstance;
 import org.eclipse.jem.internal.instantiation.base.JavaInstantiation;
 
+import org.eclipse.ve.internal.cde.emf.EditPartAdapterRunnable;
+
 import org.eclipse.ve.internal.java.core.JavaBeanTreeEditPart;
 
-
+/**
+ * MenuItem Tree Editpart.
+ * 
+ * @since 1.1.0.1
+ */
 public class MenuItemTreeEditPart extends JavaBeanTreeEditPart {
 
-	protected EReference menu;
+	protected EReference sf_menu;
 	
 	public MenuItemTreeEditPart(Object model) {
 		super(model);
@@ -40,14 +48,36 @@ public class MenuItemTreeEditPart extends JavaBeanTreeEditPart {
 		super.setModel(model);
 
 		ResourceSet rset = ((IJavaObjectInstance) model).eResource().getResourceSet();
-		menu = JavaInstantiation.getReference(rset, SWTConstants.SF_MENUITEM_MENU);
+		sf_menu = JavaInstantiation.getReference(rset, SWTConstants.SF_MENUITEM_MENU);
 	}	
 	
 	protected List getChildJavaBeans() {
-		if (((EObject) getModel()).eIsSet(menu)) {
-			return Collections.singletonList (((EObject) getModel()).eGet(menu));
+		if (((EObject) getModel()).eIsSet(sf_menu)) {
+			return Collections.singletonList (((EObject) getModel()).eGet(sf_menu));
 		} else 	
 			return Collections.EMPTY_LIST;
 	}
+	
+	protected Adapter menuItemAdapter = new EditPartAdapterRunnable(this) {
+		protected void doRun() {
+			refreshChildren();
+		}
+
+		public void notifyChanged(Notification notification) {
+			if (notification.getFeature() == sf_menu)
+				queueExec(MenuItemTreeEditPart.this, "MENU"); //$NON-NLS-1$
+		}
+	};
+
+	public void activate() {
+		super.activate();
+		((EObject) getModel()).eAdapters().add(menuItemAdapter);
+	}
+
+	public void deactivate() {
+		super.deactivate();
+		((EObject) getModel()).eAdapters().remove(menuItemAdapter);
+	}
+	
 
 }
