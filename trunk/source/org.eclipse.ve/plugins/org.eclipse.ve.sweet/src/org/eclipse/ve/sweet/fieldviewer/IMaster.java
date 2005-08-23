d66eb46c4bff54419c0fef1ce88dec0e56141b92
dorme <dorme@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-08-23 05:29:43 +0000

    Fixed Javadoc

--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet/fieldviewer/IMaster.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet/fieldviewer/IMaster.java
@@ -10,13 +10,21 @@ import org.eclipse.ve.sweet.objectviewer.IObjectViewer;
  */
 public interface IMaster {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ve.sweet.fieldviewer.IFieldViewer#addDetailViewer(org.eclipse.ve.sweet.objectviewer.IObjectViewer)
+	/**
+	 * Method addDetailViewer.  Add the specified IObjectViewer to the set of IObjectViewers whose
+	 * input will be refreshed when the "currently focused object" changes.  The concept of the 
+	 * "currently focused object" is implementation-dependent.
+	 *
+	 * @param detailViewer The IObjectViewer to add.
 	 */
 	public void addDetailViewer(IObjectViewer detailViewer);
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ve.sweet.fieldviewer.IFieldViewer#removeDetailViewer(org.eclipse.ve.sweet.objectviewer.IObjectViewer)
+	/**
+	 * Method addDetailViewer.  Removes the specified IObjectViewer from the set of IObjectViewers whose
+	 * input will be refreshed when the "currently focused object" changes.  The concept of the 
+	 * "currently focused object" is implementation-dependent.
+	 *
+	 * @param detailViewer The IObjectViewer to remove.
 	 */
 	public void removeDetailViewer(IObjectViewer detailViewer);
 
