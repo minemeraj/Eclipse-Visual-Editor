fcedb3703ea75ea12837ead3b3dc7b382de2e8d9
winchest <winchest@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-08-30 12:05:56 +0000

    *** empty log message ***

--- source/org.eclipse.ve/plugins/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet2/ListContentProvider.java
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.sweet/src/org/eclipse/ve/sweet2/ListContentProvider.java
@@ -8,14 +8,29 @@ import java.util.List;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
-public class ListContentProvider implements IStructuredContentProvider {
+public class ListContentProvider extends AbstractObjectContentProvider implements IStructuredContentProvider {
+	
+	public ListContentProvider(){
+		super(null);
+	}
+
+	public ListContentProvider(String aPropertyName) {
+		super(aPropertyName);
+	}
+
 	public Object[] getElements(Object inputElement) {
-		return ((List)inputElement).toArray();
+		if(inputElement instanceof List){
+			return ((List)inputElement).toArray();
+		} else {
+			Object result = lookupPropertyValue(inputElement);
+			if(result instanceof List){
+				return ((List)result).toArray();
+			} else {
+				return (Object[])result;
+			}
+		}
 	}
 
-	public void dispose() {				
-	}
-
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-	}
+	
+	
 }
