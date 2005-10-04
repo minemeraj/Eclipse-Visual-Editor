578a31987ea7b132d47aa8068d4a8e3e670b7e50
rkulp <rkulp@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-10-04 18:08:42 +0000

    [48550] XML View now being updated when codegen makes change.

--- source/org.eclipse.ve.java.core/codegen/org/eclipse/ve/internal/java/codegen/editorpart/XMLViewPart.java
+++ source/org.eclipse.ve.java.core/codegen/org/eclipse/ve/internal/java/codegen/editorpart/XMLViewPart.java
@@ -10,16 +10,16 @@
  *******************************************************************************/
 /*
  *  $RCSfile: XMLViewPart.java,v $
- *  $Revision: 1.3 $  $Date: 2005-10-04 17:04:41 $ 
+ *  $Revision: 1.4 $  $Date: 2005-10-04 18:08:42 $ 
  */
 package org.eclipse.ve.internal.java.codegen.editorpart;
 
 import java.io.ByteArrayOutputStream;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.XMLResource;
-import org.eclipse.gef.commands.CommandStackListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.ui.IWorkbenchPart;
@@ -28,36 +28,48 @@ import org.eclipse.ui.views.contentoutline.ContentOutline;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 
 import org.eclipse.ve.internal.cde.core.EditDomain;
+import org.eclipse.ve.internal.cde.core.ModelChangeController;
+import org.eclipse.ve.internal.cde.core.ModelChangeController.ModelChangeEvent;
 
 import org.eclipse.ve.internal.java.core.XMLTextPage;
- 
 
+/**
+ * XML ViewPart to show Java Visual Editor's xml.
+ * 
+ * @since 1.2.0
+ */
 public class XMLViewPart extends ContentOutline {
-	
+
 	public class XMLViewTextPage extends XMLTextPage implements IContentOutlinePage {
+
 		protected Map XML_SAVE_CACHE_OPTIONS = new HashMap(3);
+
 		protected EditDomain domain;
-		protected CommandStackListener stackListener = new CommandStackListener(){
-			public void commandStackChanged(EventObject event) {
-				refresh();
-			}	        	
-        };
-        
+
+		protected ModelChangeController.ModelChangeListener changeListener = new ModelChangeController.ModelChangeListener() {
+
+			public void transactionEvent(ModelChangeEvent event) {
+				if (event.getEvent() == ModelChangeEvent.TRANSACTION_COMPLETED)
+					refresh();
+			}
+		};
+
 		public XMLViewTextPage(EditDomain anEditDomain) {
 			domain = anEditDomain;
 			XML_SAVE_CACHE_OPTIONS.put(XMLResource.OPTION_PROCESS_DANGLING_HREF, XMLResource.OPTION_PROCESS_DANGLING_HREF_RECORD);
 			XML_SAVE_CACHE_OPTIONS.put(XMLResource.OPTION_LINE_WIDTH, new Integer(100));
-	        XML_SAVE_CACHE_OPTIONS.put(XMLResource.OPTION_ENCODING, "UTF-8");//$NON-NLS-1$	
-	        domain.getCommandStack().addCommandStackListener(stackListener);
+			XML_SAVE_CACHE_OPTIONS.put(XMLResource.OPTION_ENCODING, "UTF-8");//$NON-NLS-1$	
+			((ModelChangeController) domain.getData(ModelChangeController.MODEL_CHANGE_CONTROLLER_KEY)).addModelChangeListener(changeListener);
 		}
-		
+
 		public void dispose() {
-			domain.getCommandStack().removeCommandStackListener(stackListener);
+			((ModelChangeController) domain.getData(ModelChangeController.MODEL_CHANGE_CONTROLLER_KEY)).removeModelChangeListener(changeListener);
 			super.dispose();
 		}
+
 		public void refresh() {
-			if(domain.getDiagramData() != null){
-				Resource resource = domain.getDiagramData().eResource(); 
+			if (domain.getDiagramData() != null) {
+				Resource resource = domain.getDiagramData().eResource();
 				ByteArrayOutputStream os = new ByteArrayOutputStream();
 				try {
 					resource.save(os, XML_SAVE_CACHE_OPTIONS);
@@ -67,36 +79,40 @@ public class XMLViewPart extends ContentOutline {
 				}
 			}
 		}
-		public void addSelectionChangedListener(ISelectionChangedListener listener) {			
+
+		public void addSelectionChangedListener(ISelectionChangedListener listener) {
 		}
+
 		public ISelection getSelection() {
 			return null;
 		}
-		public void removeSelectionChangedListener(ISelectionChangedListener listener) {			
+
+		public void removeSelectionChangedListener(ISelectionChangedListener listener) {
 		}
-		public void setSelection(ISelection selection) {			
+
+		public void setSelection(ISelection selection) {
 		}
 	}
 
 	protected PageRec doCreatePage(IWorkbenchPart part) {
 		// Try to get a resource
-		if(part instanceof JavaVisualEditorPart){
-			EditDomain domain = ((JavaVisualEditorPart)part).getEditDomain();
+		if (part instanceof JavaVisualEditorPart) {
+			EditDomain domain = ((JavaVisualEditorPart) part).getEditDomain();
 			// Create an XMLTextPage
 			XMLViewTextPage page = new XMLViewTextPage(domain);
 			page.refresh();
- 			initPage(page);
+			initPage(page);
 			page.createControl(getPageBook());
 			return new PageRec(part, page);
 		}
 		// There is no beans list
 		return null;
 	}
-	
+
 	protected IPage createDefaultPage(PageBook book) {
 		MessagePage msgPage = (MessagePage) super.createDefaultPage(book);
-		msgPage.setMessage("For use with the Visual Editor"); 
+		msgPage.setMessage("For use with the Visual Editor");
 		return msgPage;
-	}	
+	}
 
 }
