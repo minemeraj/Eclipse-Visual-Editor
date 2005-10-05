e981ece3b02e8824e0624fb6831086249ea90760
gmendel <gmendel@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-10-05 15:57:16 +0000

    Browser based XMI view

--- trunk/source/org.eclipse.ve.java.core/core/org/eclipse/ve/internal/java/core/XMLTextPage.java
+++ trunk/source/org.eclipse.ve.java.core/core/org/eclipse/ve/internal/java/core/XMLTextPage.java
@@ -11,32 +11,62 @@
 package org.eclipse.ve.internal.java.core;
 /*
  *  $RCSfile: XMLTextPage.java,v $
- *  $Revision: 1.5 $  $Date: 2005-09-16 14:22:47 $ 
+ *  $Revision: 1.6 $  $Date: 2005-10-05 15:57:16 $ 
  */
 
 
 
-import org.eclipse.jface.text.ITextSelection;
+import java.io.*;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.ui.part.IPageBookViewPage;
 import org.eclipse.ui.part.Page;
 
 public class XMLTextPage extends Page implements IPageBookViewPage {
-	protected XMLTextViewer viewer;
+//	protected XMLTextViewer viewer;
+	protected Browser browser;
 	protected String fText = ""; //$NON-NLS-1$
 	
+
+File tempFile = null;	
+private void setBrowserText(String text) {
+	// The SWT widget does not know how to render
+	// XML from a buffer (setText())... it uses the file's extension to figure out
+	// that it is an XML mime type.
+	try {
+		File old = tempFile;
+		tempFile = File.createTempFile("VE_XMI",".xml");
+		Writer output = new BufferedWriter( new FileWriter(tempFile) );
+		output.write( text );
+		output.close();
+		browser.setUrl(tempFile.toURL().toExternalForm());
+		if (old != null)
+		   old.delete();
+	} catch (IOException e) {
+		JavaVEPlugin.log(e);
+	}
+    
+	
+	
+}
 public void createControl(Composite parent) {
-	viewer = new XMLTextViewer(parent , SWT.READ_ONLY | SWT.V_SCROLL | SWT.H_SCROLL);
-	viewer.setText(fText);
+//	viewer = new XMLTextViewer(parent , SWT.READ_ONLY | SWT.V_SCROLL | SWT.H_SCROLL);
+	browser = new Browser(parent, SWT.NONE);	
+	
+//	viewer.setText(fText);
+	setBrowserText(fText);
 	fText = null;	// Get rid of it since now handled through viewer.
 }		
 
 public void setText(final String aString){
-	if ( viewer != null ) {
+//	if ( viewer != null ) {
+	if ( browser != null ) {	
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
-				viewer.setText(aString);
+				//browser.setText(aString);
+				setBrowserText(aString);
 			}
 		}); 
 	} else {
@@ -45,14 +75,17 @@ public void setText(final String aString){
 }
 
 public String getSelectedText(){
-	return ((ITextSelection)viewer.getSelection()).getText();
+	//return ((ITextSelection)viewer.getSelection()).getText();
+	return null;
 }
 
 public Control getControl() {
-	return viewer != null ? viewer.getControl() : null;
+	// return viewer != null ? viewer.getControl() : null;
+	return browser;
 }
 
 public void setFocus() {
-	viewer.getControl().setFocus();
+	// viewer.getControl().setFocus();
+	browser.setFocus();
 }
 }
