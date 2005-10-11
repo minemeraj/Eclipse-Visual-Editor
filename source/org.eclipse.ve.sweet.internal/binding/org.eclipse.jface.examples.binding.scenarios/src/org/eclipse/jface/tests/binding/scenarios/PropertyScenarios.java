748ff58d48dcf29cf091476dea8a8f64eff8d861
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-10-11 22:12:43 +0000

    Added an example scenario test, see the Javadoc for how to run it. Test is green. Just doesn't work quite the way I want it yet: I would like to be able to see visually what's going on, but so far there is nothing to see.

---
+++ source/org.eclipse.ve.sweet.internal/binding/org.eclipse.jface.examples.binding.scenarios/src/org/eclipse/jface/tests/binding/scenarios/PropertyScenarios.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jface.tests.binding.scenarios;
+
+import org.eclipse.jface.binding.BindingException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.examples.rcp.adventure.Adventure;
+import org.eclipse.ui.examples.rcp.binding.scenarios.SampleData;
+
+/**
+ * To run the tests in this class, right-click and select "Run As JUnit Plug-in
+ * Test". This will also start an Eclipse instance. To clean up the launch
+ * configuration, open up its "Main" tab and select "[No Application] - Headless
+ * Mode" as the application to run.
+ */
+
+public class PropertyScenarios extends ScenariosTestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		// do any setup work here
+	}
+
+	protected void tearDown() throws Exception {
+		// do any teardown work here
+		super.tearDown();
+	}
+
+	public void testScenario01() throws BindingException {
+		// Binding the name property of an Adventure object to the contents of
+		// Text controls, no conversion, no validation.
+		Adventure adventure = SampleData.WINTER_HOLIDAY;
+		Text text = new Text(getComposite(), SWT.BORDER);
+		getDbs().bindValue(text, "text", adventure, "name");
+		spinEventLoop(1);
+		assertEquals(adventure.getName(), text.getText());
+		text.setText("foobar");
+		spinEventLoop(1);
+		assertEquals("foobar", adventure.getName());
+		adventure.setName("barfoo");
+		spinEventLoop(1);
+		assertEquals("barfoo", text.getText());
+	}
+
+}
