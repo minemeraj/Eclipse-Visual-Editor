57e69c358ca782fb47d0a9d0bed73cfb025a3065
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-10-17 22:16:20 +0000

    added some more scenario tests

--- trunk/source/org.eclipse.ve.sweet.internal/binding/org.eclipse.jface.examples.binding.scenarios/src/org/eclipse/jface/tests/binding/scenarios/PropertyScenarios.java
+++ trunk/source/org.eclipse.ve.sweet.internal/binding/org.eclipse.jface.examples.binding.scenarios/src/org/eclipse/jface/tests/binding/scenarios/PropertyScenarios.java
@@ -11,8 +11,11 @@
 package org.eclipse.jface.tests.binding.scenarios;
 
 import org.eclipse.jface.binding.BindingException;
+import org.eclipse.jface.binding.Converter;
 import org.eclipse.jface.binding.IConverter;
 import org.eclipse.jface.binding.IUpdatableValue;
+import org.eclipse.jface.binding.IValidator;
+import org.eclipse.jface.binding.IdentityConverter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.examples.rcp.adventure.Adventure;
@@ -89,7 +92,8 @@ public class PropertyScenarios extends ScenariosTestCase {
 			public Object convertModel(Object object) {
 				return new Integer((String) object);
 			}
-			public Object convertTarget(Object object){
+
+			public Object convertTarget(Object object) {
 				return object.toString();
 			}
 		});
@@ -144,5 +148,122 @@ public class PropertyScenarios extends ScenariosTestCase {
 		assertEquals(adventure.getDefaultLodging().getDescription(), text
 				.getText());
 
+	}
+
+	public void testScenario05() throws BindingException {
+		// Binding the name property of an Adventure object to the contents of
+		// Text controls where conversion occurs – the model data is held all in
+		// uppercase and displayed in lowercase with the first letter
+		// capitalized.
+		Text text = new Text(getComposite(), SWT.BORDER);
+		adventure.setName("UPPERCASE");
+		getDbs().bindValue(text, "text", adventure, "name", new IConverter() {
+			public Class getModelType() {
+				return String.class;
+			}
+
+			public Class getTargetType() {
+				return String.class;
+			}
+
+			public Object convertModel(Object fromObject) {
+				return ((String) fromObject).toUpperCase();
+			}
+
+			public Object convertTarget(Object toObject) {
+				String modelValue = (String) toObject;
+				if (modelValue == null || modelValue.equals("")) {
+					return modelValue;
+				}
+				String firstChar = modelValue.substring(0, 1);
+				String remainingChars = modelValue.substring(1);
+				return firstChar.toUpperCase() + remainingChars.toLowerCase();
+			}
+		});
+		// spinEventLoop(1);
+		assertEquals("Uppercase", text.getText());
+		text.setText("lowercase");
+		// spinEventLoop(1);
+		// TODO If we wanted to "canonicalize" the value in the text field, how
+		// could we do that?
+		assertEquals("LOWERCASE", adventure.getName());
+	}
+
+	public void testScenario06() throws BindingException {
+		// Binding the name property of an Adventure object to the contents of
+		// Text controls where validation occurs and the name cannot be longer
+		// than 15 characters and cannot contain spaces
+		Text text = new Text(getComposite(), SWT.BORDER);
+		final String noSpacesMessage = "Name must not contain spaces.";
+		final String max15CharactersMessage = "Maximum length for name is 15 characters.";
+		adventure.setName("ValidValue");
+		getDbs().bindValue(text, "text", adventure, "name",
+				new IdentityConverter(String.class), new IValidator() {
+					public String isPartiallyValid(Object value) {
+						return isValid(value);
+					}
+
+					public String isValid(Object value) {
+						String stringValue = (String) value;
+						if (stringValue.length() > 15) {
+							return max15CharactersMessage;
+						} else if (stringValue.indexOf(' ') != -1) {
+							return noSpacesMessage;
+						} else {
+							return null;
+						}
+					}
+				});
+		// no validation message
+		assertEquals("", getDbs().getCombinedValidationMessage().getValue());
+		text.setText("Invalid Value");
+		assertEquals(noSpacesMessage, getDbs().getCombinedValidationMessage()
+				.getValue());
+		assertEquals("ValidValue", text.getText());
+		text.setText("InvalidValueBecauseTooLong");
+		assertEquals(max15CharactersMessage, getDbs()
+				.getCombinedValidationMessage().getValue());
+		assertEquals("ValidValue", text.getText());
+		text.setText("anothervalid");
+		assertEquals("", getDbs().getCombinedValidationMessage().getValue());
+		assertEquals("anothervalid", text.getText());
+		assertEquals("anothervalid", adventure.getName());
+	}
+
+	public void testScenario07() throws BindingException {
+		// Binding the price property of an Adventure to a Text control. Price
+		// is a double and Text accepts String so conversion will have to occur.
+		// Validation ensure that the value is positive
+		Text text = new Text(getComposite(), SWT.BORDER);
+		getDbs().bindValue(text, "text", adventure, "name",
+				new Converter(String.class, double.class) {
+
+					public Object convertModel(Object fromObject) {
+						return new Double((String) fromObject);
+					}
+
+					public Object convertTarget(Object toObject) {
+						return ((Double) toObject).toString();
+					}
+				}, new IValidator() {
+					public String isPartiallyValid(Object value) {
+						return null;
+					}
+
+					public String isValid(Object value) {
+						String stringValue = (String) value;
+						try {
+							double doubleValue = Double
+									.parseDouble(stringValue);
+							if (doubleValue < 0.0) {
+								return "Price cannot be negative.";
+							} else {
+								return null;
+							}
+						} catch (NumberFormatException ex) {
+							return "Price must be a number.";
+						}
+					}
+				});
 	}
 }
