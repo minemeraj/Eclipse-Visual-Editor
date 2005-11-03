9a4b0de9608ac6d0ff29fd2dd933475d0ff6e017
sgunturi <sgunturi@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-11-03 18:47:36 +0000

    Fixed test failure

--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.sweet.internal/binding/org.eclipse.jface.examples.binding.scenarios/src/org/eclipse/jface/tests/binding/scenarios/UpdatableFactoriesTest.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.sweet.internal/binding/org.eclipse.jface.examples.binding.scenarios/src/org/eclipse/jface/tests/binding/scenarios/UpdatableFactoriesTest.java
@@ -12,87 +12,121 @@
  *  Created Oct 21, 2005 by Gili Mendel
  * 
  *  $RCSfile: UpdatableFactoriesTest.java,v $
- *  $Revision: 1.4 $  $Date: 2005-11-02 22:20:54 $ 
+ *  $Revision: 1.5 $  $Date: 2005-11-03 18:47:36 $ 
  */
- 
+
 package org.eclipse.jface.tests.binding.scenarios;
 
 import java.util.Map;
 
-import org.eclipse.jface.databinding.*;
- 
+import org.eclipse.jface.databinding.BindingException;
+import org.eclipse.jface.databinding.IChangeListener;
+import org.eclipse.jface.databinding.IUpdatable;
+import org.eclipse.jface.databinding.IUpdatableFactory2;
+import org.eclipse.jface.databinding.IValidationContext;
 
 public class UpdatableFactoriesTest extends ScenariosTestCase {
 
-	
-	interface Root {};
-	interface None extends Root{};
-	interface Middle extends None {};
-	interface StandAlone{};
-	class  RootClass implements Root {};
-	class NoneClass implements None{};
-	class MiddleClass implements Middle{};
-	class AllClass implements StandAlone, Middle, Root {};
-	
-	class MiddleChild extends MiddleClass {};
-	
-    interface TestIUpdatable extends IUpdatable {
-    	public Class getType();
-    }
-	
-	class Factory implements IUpdatableFactory2{
+	interface Root {
+	};
+
+	interface None extends Root {
+	};
+
+	interface Middle extends None {
+	};
+
+	interface StandAlone {
+	};
+
+	class RootClass implements Root {
+	};
+
+	class NoneClass implements None {
+	};
+
+	class MiddleClass implements Middle {
+	};
+
+	class AllClass implements StandAlone, Middle, Root {
+	};
+
+	class MiddleChild extends MiddleClass {
+	};
+
+	interface TestIUpdatable extends IUpdatable {
+		public Class getType();
+	}
+
+	class Factory implements IUpdatableFactory2 {
 		Class c;
-		public Factory (Class c) {
+
+		public Factory(Class c) {
 			this.c = c;
 		}
-		public IUpdatable createUpdatable(Map properties, Object description, IValidationContext validationContext) {			
-			return new TestIUpdatable(){
-				public void dispose() {}			
-				public void removeChangeListener(IChangeListener changeListener) {}			
-				public void addChangeListener(IChangeListener changeListener) {}
-				public Class getType() { return c; } 
-			};
-		}	
+
+		public IUpdatable createUpdatable(Map properties, Object description,
+				IValidationContext validationContext) {
+			if (c.isInstance(description)) {
+				return new TestIUpdatable() {
+					public void dispose() {
+					}
+
+					public void removeChangeListener(
+							IChangeListener changeListener) {
+					}
+
+					public void addChangeListener(IChangeListener changeListener) {
+					}
+
+					public Class getType() {
+						return c;
+					}
+				};
+			}
+			return null;
+		}
 	}
-	
-	IUpdatableFactory2 root = new Factory(Root.class), middle = new Factory(Middle.class), sa = new Factory(StandAlone.class), factory = new Factory(Object.class);
-	
-			
+
+	IUpdatableFactory2 root = new Factory(Root.class);
+
+	IUpdatableFactory2 middle = new Factory(Middle.class);
+
+	IUpdatableFactory2 sa = new Factory(StandAlone.class);
+
+	IUpdatableFactory2 factory = new Factory(Object.class);
+
 	protected Class getFactoryType(Object src) throws BindingException {
-		TestIUpdatable u = (TestIUpdatable) getDbc().createUpdatable2(new PropertyDescription(src, "n/a"));
+		TestIUpdatable u = (TestIUpdatable) getDbc().createUpdatable2(src);
 		return u.getType();
 	}
-	
-	 public void test_factoryRegistration() throws BindingException {
-		 
-		 getDbc().addUpdatableFactory2(root);
-		 getDbc().addUpdatableFactory2(middle);
-		 
-		 // Direct mapping
-		 assertEquals(getFactoryType(new RootClass()), Root.class);
-		 assertEquals(getFactoryType(new MiddleClass()), Middle.class);
-		 
-		 // Inherent interface
-		 assertEquals(getFactoryType(new NoneClass()), Root.class);
-		 
-		 // AllClass inherent interface
-		 assertEquals(getFactoryType(new AllClass()), Middle.class);
-		 
-		 // class inheretence 
-		 assertEquals(getFactoryType(new MiddleChild()), Middle.class);
-		 
-		 // Direct, first interface
-		 getDbc().addUpdatableFactory2(sa);
-		 assertEquals(getFactoryType(new AllClass()), StandAlone.class);
-		 
-		 // Class based contribution.
-		 getDbc().addUpdatableFactory2(factory);
-		 assertEquals(getFactoryType(new AllClass()), Object.class);
-		 
-		 
-		 
-	 }
-		 
-	
+
+	public void test_factoryRegistration() throws BindingException {
+
+		getDbc().addUpdatableFactory2(root);
+		getDbc().addUpdatableFactory2(middle);
+
+		// Direct mapping
+		assertEquals(Root.class, getFactoryType(new RootClass()));
+		assertEquals(Middle.class, getFactoryType(new MiddleClass()));
+
+		// Inherent interface
+		assertEquals(Root.class, getFactoryType(new NoneClass()));
+
+		// AllClass inherent interface
+		assertEquals(Middle.class, getFactoryType(new AllClass()));
+
+		// class inheretence
+		assertEquals(Middle.class, getFactoryType(new MiddleChild()));
+
+		// Direct, first interface
+		getDbc().addUpdatableFactory2(sa);
+		assertEquals(StandAlone.class, getFactoryType(new AllClass()));
+
+		// Class based contribution.
+		getDbc().addUpdatableFactory2(factory);
+		assertEquals(Object.class, getFactoryType(new AllClass()));
+
+	}
 
 }
