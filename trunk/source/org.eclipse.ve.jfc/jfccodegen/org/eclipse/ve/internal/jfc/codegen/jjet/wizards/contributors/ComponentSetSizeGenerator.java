62fdbb0d19f0e021789257c46c1748fb89b6f892
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-11-29 22:06:31 +0000

    110145 - generate setLayout code based on VE layout preferences

--- trunk/source/org.eclipse.ve.jfc/jfccodegen/org/eclipse/ve/internal/jfc/codegen/jjet/wizards/contributors/ComponentSetSizeGenerator.java
+++ trunk/source/org.eclipse.ve.jfc/jfccodegen/org/eclipse/ve/internal/jfc/codegen/jjet/wizards/contributors/ComponentSetSizeGenerator.java
@@ -25,20 +25,25 @@ public class ComponentSetSizeGenerator implements org.eclipse.ve.internal.java.c
   }
 
   protected final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-  protected final String TEXT_1 = NL + "public class ";
-  protected final String TEXT_2 = " {" + NL + "" + NL + "\t/**" + NL + "\t * This is the default constructor" + NL + "\t */" + NL + "\tpublic ";
-  protected final String TEXT_3 = "() {" + NL + "\t\tsuper(); \t" + NL + "\t";
-  protected final String TEXT_4 = "\tinitialize();" + NL + "\t";
-  protected final String TEXT_5 = "}" + NL + "" + NL + "\t" + NL + "\t/**" + NL + "\t * This method initializes this" + NL + "\t * " + NL + "\t * @return void" + NL + "\t */" + NL + "\t";
-  protected final String TEXT_6 = "public ";
-  protected final String TEXT_7 = "private ";
-  protected final String TEXT_8 = " void ";
-  protected final String TEXT_9 = "init()";
-  protected final String TEXT_10 = "initialize()";
-  protected final String TEXT_11 = " {" + NL + "\t\tthis.setSize(300,200);";
-  protected final String TEXT_12 = NL + "\t\tthis.setTitle(\"";
-  protected final String TEXT_13 = "\");";
-  protected final String TEXT_14 = NL + "\t}" + NL + "}";
+  protected final String TEXT_1 = "import ";
+  protected final String TEXT_2 = ";" + NL + "" + NL + "public class ";
+  protected final String TEXT_3 = " {" + NL + "" + NL + "\t/**" + NL + "\t * This is the default constructor" + NL + "\t */" + NL + "\tpublic ";
+  protected final String TEXT_4 = "() {" + NL + "\t\tsuper(); \t" + NL + "\t";
+  protected final String TEXT_5 = "\tinitialize();" + NL + "\t";
+  protected final String TEXT_6 = "}" + NL + "" + NL + "\t" + NL + "\t/**" + NL + "\t * This method initializes this" + NL + "\t * " + NL + "\t * @return void" + NL + "\t */" + NL + "\t";
+  protected final String TEXT_7 = "public ";
+  protected final String TEXT_8 = "private ";
+  protected final String TEXT_9 = " void ";
+  protected final String TEXT_10 = "init()";
+  protected final String TEXT_11 = "initialize()";
+  protected final String TEXT_12 = " {" + NL + "\t\tthis.setSize(300,200);";
+  protected final String TEXT_13 = NL + "\t\tthis.setTitle(\"";
+  protected final String TEXT_14 = "\");";
+  protected final String TEXT_15 = NL + "\t";
+  protected final String TEXT_16 = "this.setLayout(null);";
+  protected final String TEXT_17 = "this.setLayout(new ";
+  protected final String TEXT_18 = "());";
+  protected final String TEXT_19 = NL + "\t}" + NL + "}";
 
 public String generateSource(String typeName, String superClassName, HashMap argumentMatrix)
   {
@@ -48,47 +53,72 @@ public String generateSource(String typeName, String superClassName, HashMap arg
  * This was created from the javajet file: 
  */
 
+    
+	String layout = org.eclipse.ve.internal.jfc.core.JFCVisualPlugin.getPlugin().getPluginPreferences().getString(org.eclipse.ve.internal.jfc.core.JFCVisualPlugin.DEFAULT_LAYOUTMANAGER);
+	boolean isNullLayout = layout.equals(org.eclipse.ve.internal.jfc.core.JFCVisualPlugin.NULL_LAYOUT);
+	String layoutName = "null";
+	if (!isNullLayout)
+		layoutName = layout.substring(layout.lastIndexOf('.')+1, layout.length());
+
+    if("java.awt.Panel".equals(superClassName) || "javax.swing.JPanel".equals(superClassName))
     stringBuffer.append(TEXT_1);
-    stringBuffer.append(typeName);
+    stringBuffer.append(layout);
     stringBuffer.append(TEXT_2);
     stringBuffer.append(typeName);
     stringBuffer.append(TEXT_3);
+    stringBuffer.append(typeName);
+    stringBuffer.append(TEXT_4);
     		if(!"java.applet.Applet".equals(superClassName)){
 	
-    stringBuffer.append(TEXT_4);
+    stringBuffer.append(TEXT_5);
     		}
 	
-    stringBuffer.append(TEXT_5);
+    stringBuffer.append(TEXT_6);
     
 		if("java.applet.Applet".equals(superClassName)){
 			
-    stringBuffer.append(TEXT_6);
+    stringBuffer.append(TEXT_7);
     
 		}else{
 			
-    stringBuffer.append(TEXT_7);
+    stringBuffer.append(TEXT_8);
     
 		}
-    stringBuffer.append(TEXT_8);
+    stringBuffer.append(TEXT_9);
     
 			if("java.applet.Applet".equals(superClassName)){
 				
-    stringBuffer.append(TEXT_9);
+    stringBuffer.append(TEXT_10);
     
 			}else{
 				
-    stringBuffer.append(TEXT_10);
-    
-		}
     stringBuffer.append(TEXT_11);
     
-	if ("java.awt.Frame".equals(superClassName)) {
+		}
     stringBuffer.append(TEXT_12);
-    stringBuffer.append(superClassName.substring(superClassName.lastIndexOf(".")+1));
+    
+	if ("java.awt.Frame".equals(superClassName)) {
     stringBuffer.append(TEXT_13);
+    stringBuffer.append(superClassName.substring(superClassName.lastIndexOf(".")+1));
+    stringBuffer.append(TEXT_14);
     
 	}
-    stringBuffer.append(TEXT_14);
+    stringBuffer.append(TEXT_15);
+    
+	if("java.awt.Panel".equals(superClassName) || "javax.swing.JPanel".equals(superClassName)) {
+		if (isNullLayout){
+		
+    stringBuffer.append(TEXT_16);
+    
+		} else {
+		
+    stringBuffer.append(TEXT_17);
+    stringBuffer.append(layoutName);
+    stringBuffer.append(TEXT_18);
+    
+		}
+	}
+    stringBuffer.append(TEXT_19);
     return stringBuffer.toString();
   }
 }
--- trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/swtcodegen/org/eclipse/ve/internal/swt/codegen/jjet/wizards/contributors/CompositeSourceGenerator.java
+++ trunk/source/org.eclipse.ve/plugins/org.eclipse.ve.swt/swtcodegen/org/eclipse/ve/internal/swt/codegen/jjet/wizards/contributors/CompositeSourceGenerator.java
@@ -27,13 +27,18 @@ public class CompositeSourceGenerator implements org.eclipse.ve.internal.java.co
   protected final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "";
   protected final String TEXT_2 = NL + "import org.eclipse.swt.layout.FillLayout;" + NL + "import org.eclipse.swt.widgets.Shell;" + NL + "import org.eclipse.swt.widgets.Display;" + NL + "import org.eclipse.swt.SWT;";
-  protected final String TEXT_3 = NL + "import org.eclipse.swt.graphics.Point;" + NL + "" + NL + "" + NL + "public class ";
-  protected final String TEXT_4 = " extends Composite {" + NL + "" + NL + "\tpublic ";
-  protected final String TEXT_5 = "(Composite parent, int style) {" + NL + "\t\tsuper(parent, style);" + NL + "\t\tinitialize();" + NL + "\t}" + NL + "\tprivate void initialize() {" + NL + "\t\tsetSize(new Point(300,200));" + NL + "\t}" + NL + "}";
-  protected final String TEXT_6 = NL + "\tpublic static void main(String[] args) {" + NL + "\t\t/* Before this is run, be sure to set up the launch configuration (Arguments->VM Arguments)" + NL + "\t\t * for the correct SWT library path in order to run with the SWT dlls. " + NL + "\t\t * The dlls are located in the SWT plugin jar.  " + NL + "\t\t * For example, on Windows the Eclipse SWT 3.1 plugin jar is:" + NL + "\t\t *       installation_directory\\plugins\\org.eclipse.swt.win32_3.1.0.jar" + NL + "\t\t */" + NL + "\t\tDisplay display = Display.getDefault();\t\t" + NL + "\t\tShell shell = new Shell(display);" + NL + "\t\tshell.setLayout(new FillLayout());" + NL + "\t\tshell.setSize(new Point(300,200));" + NL + "\t\t";
-  protected final String TEXT_7 = " thisClass = new ";
-  protected final String TEXT_8 = "(shell, SWT.NONE);" + NL + "\t\tshell.open();" + NL + "\t\t" + NL + "\t\twhile (!shell.isDisposed()) {" + NL + "\t\t\tif (!display.readAndDispatch()) display.sleep ();" + NL + "\t\t}" + NL + "\t\tdisplay.dispose();\t\t" + NL + "\t}";
-  protected final String TEXT_9 = NL;
+  protected final String TEXT_3 = NL + "import org.eclipse.swt.graphics.Point;";
+  protected final String TEXT_4 = NL + "import ";
+  protected final String TEXT_5 = ";";
+  protected final String TEXT_6 = NL + NL + "public class ";
+  protected final String TEXT_7 = " extends Composite {" + NL + "" + NL + "\tpublic ";
+  protected final String TEXT_8 = "(Composite parent, int style) {" + NL + "\t\tsuper(parent, style);" + NL + "\t\tinitialize();" + NL + "\t}" + NL + "\tprivate void initialize() {" + NL + "\t\tsetSize(new Point(300,200));\t";
+  protected final String TEXT_9 = "\t" + NL + "\t\tsetLayout(new ";
+  protected final String TEXT_10 = "());";
+  protected final String TEXT_11 = NL + "\t}" + NL + "}";
+  protected final String TEXT_12 = NL + "\tpublic static void main(String[] args) {" + NL + "\t\t/* Before this is run, be sure to set up the launch configuration (Arguments->VM Arguments)" + NL + "\t\t * for the correct SWT library path in order to run with the SWT dlls. " + NL + "\t\t * The dlls are located in the SWT plugin jar.  " + NL + "\t\t * For example, on Windows the Eclipse SWT 3.1 plugin jar is:" + NL + "\t\t *       installation_directory\\plugins\\org.eclipse.swt.win32_3.1.0.jar" + NL + "\t\t */" + NL + "\t\tDisplay display = Display.getDefault();\t\t" + NL + "\t\tShell shell = new Shell(display);" + NL + "\t\tshell.setLayout(new FillLayout());" + NL + "\t\tshell.setSize(new Point(300,200));" + NL + "\t\tnew ";
+  protected final String TEXT_13 = "(shell, SWT.NONE);" + NL + "\t\tshell.open();" + NL + "\t\t" + NL + "\t\twhile (!shell.isDisposed()) {" + NL + "\t\t\tif (!display.readAndDispatch()) display.sleep ();" + NL + "\t\t}" + NL + "\t\tdisplay.dispose();\t\t" + NL + "\t}";
+  protected final String TEXT_14 = NL;
 
 public String generateSource(String typeName, String superClassName, HashMap argumentMatrix)
   {
@@ -45,26 +50,41 @@ public String generateSource(String typeName, String superClassName, HashMap arg
 
     
 	boolean createMain = (argumentMatrix != null && ((String)argumentMatrix.get(CREATE_MAIN)).equals("true"));
+	String layout = org.eclipse.ve.internal.swt.SwtPlugin.getDefault().getPluginPreferences().getString(org.eclipse.ve.internal.swt.SwtPlugin.DEFAULT_LAYOUT);
+	boolean isNullLayout = layout.equals(org.eclipse.ve.internal.swt.SwtPlugin.NULL_LAYOUT);
+	String layoutName = "";
+	if (!isNullLayout)
+		layoutName = layout.substring(layout.lastIndexOf('.')+1, layout.length());
 	 
     stringBuffer.append(TEXT_1);
     if(createMain){
     stringBuffer.append(TEXT_2);
     }
     stringBuffer.append(TEXT_3);
-    stringBuffer.append(typeName);
+    if(!isNullLayout){
     stringBuffer.append(TEXT_4);
-    stringBuffer.append(typeName);
+    stringBuffer.append(layout);
     stringBuffer.append(TEXT_5);
-     	
-	if (createMain) {
+    }
     stringBuffer.append(TEXT_6);
     stringBuffer.append(typeName);
     stringBuffer.append(TEXT_7);
     stringBuffer.append(typeName);
     stringBuffer.append(TEXT_8);
+    if(!isNullLayout){
+    stringBuffer.append(TEXT_9);
+    stringBuffer.append(layoutName);
+    stringBuffer.append(TEXT_10);
+    }
+    stringBuffer.append(TEXT_11);
+     	
+	if (createMain) {
+    stringBuffer.append(TEXT_12);
+    stringBuffer.append(typeName);
+    stringBuffer.append(TEXT_13);
     
 	}
-    stringBuffer.append(TEXT_9);
+    stringBuffer.append(TEXT_14);
     return stringBuffer.toString();
   }
 }
