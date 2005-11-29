e1a2eb92a06f17aa2d84e55d51da8ad3566bc74a
pwalker <pwalker@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-11-29 22:06:31 +0000

    110145 - generate setLayout code based on VE layout preferences

--- source/org.eclipse.ve/plugins/org.eclipse.ve.swt/swtcodegen/org/eclipse/ve/internal/swt/codegen/jjet/wizards/contributors/ShellSourceGenerator.java
+++ source/org.eclipse.ve/plugins/org.eclipse.ve.swt/swtcodegen/org/eclipse/ve/internal/swt/codegen/jjet/wizards/contributors/ShellSourceGenerator.java
@@ -27,13 +27,19 @@ public class ShellSourceGenerator implements org.eclipse.ve.internal.java.codege
   protected final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "";
   protected final String TEXT_2 = NL + "import org.eclipse.swt.widgets.Display;";
-  protected final String TEXT_3 = NL + "import org.eclipse.swt.widgets.Shell;" + NL + "import org.eclipse.swt.graphics.Point;" + NL + "" + NL + "public class ";
-  protected final String TEXT_4 = " {" + NL + "" + NL + "\tprivate Shell sShell = null;" + NL + "\t" + NL + "\t/**" + NL + "\t * This method initializes sShell" + NL + "\t */" + NL + "\tprivate void createSShell() {" + NL + "\t\tsShell = new Shell();\t\t   " + NL + "\t\tsShell.setText(\"";
-  protected final String TEXT_5 = "\");" + NL + "\t\tsShell.setSize(new Point(300,200));" + NL + "\t}" + NL + "}";
-  protected final String TEXT_6 = NL + "\tpublic static void main(String[] args) {" + NL + "\t\t/* Before this is run, be sure to set up the launch configuration (Arguments->VM Arguments)" + NL + "\t\t * for the correct SWT library path in order to run with the SWT dlls. " + NL + "\t\t * The dlls are located in the SWT plugin jar.  " + NL + "\t\t * For example, on Windows the Eclipse SWT 3.1 plugin jar is:" + NL + "\t\t *       installation_directory\\plugins\\org.eclipse.swt.win32_3.1.0.jar" + NL + "\t\t */" + NL + "\t\tDisplay display = Display.getDefault();\t\t" + NL + "\t\t";
-  protected final String TEXT_7 = " thisClass = new ";
-  protected final String TEXT_8 = "();" + NL + "\t\tthisClass.createSShell() ;" + NL + "\t\tthisClass.sShell.open();" + NL + "\t\t" + NL + "\t\twhile (!thisClass.sShell.isDisposed()) {" + NL + "\t\t\tif (!display.readAndDispatch()) display.sleep ();" + NL + "\t\t}" + NL + "\t\tdisplay.dispose();\t\t" + NL + "\t}";
-  protected final String TEXT_9 = NL;
+  protected final String TEXT_3 = NL + "import org.eclipse.swt.widgets.Shell;" + NL + "import org.eclipse.swt.graphics.Point;";
+  protected final String TEXT_4 = NL + "import ";
+  protected final String TEXT_5 = ";";
+  protected final String TEXT_6 = NL + NL + "public class ";
+  protected final String TEXT_7 = " {" + NL + "" + NL + "\tprivate Shell sShell = null;" + NL + "\t" + NL + "\t/**" + NL + "\t * This method initializes sShell" + NL + "\t */" + NL + "\tprivate void createSShell() {" + NL + "\t\tsShell = new Shell();\t\t   " + NL + "\t\tsShell.setText(\"";
+  protected final String TEXT_8 = "\");" + NL + "\t\tsShell.setSize(new Point(300,200));\t";
+  protected final String TEXT_9 = "\t" + NL + "\t\tsShell.setLayout(new ";
+  protected final String TEXT_10 = "());";
+  protected final String TEXT_11 = NL + "\t}" + NL + "}";
+  protected final String TEXT_12 = NL + "\tpublic static void main(String[] args) {" + NL + "\t\t/* Before this is run, be sure to set up the launch configuration (Arguments->VM Arguments)" + NL + "\t\t * for the correct SWT library path in order to run with the SWT dlls. " + NL + "\t\t * The dlls are located in the SWT plugin jar.  " + NL + "\t\t * For example, on Windows the Eclipse SWT 3.1 plugin jar is:" + NL + "\t\t *       installation_directory\\plugins\\org.eclipse.swt.win32_3.1.0.jar" + NL + "\t\t */" + NL + "\t\tDisplay display = Display.getDefault();\t\t" + NL + "\t\t";
+  protected final String TEXT_13 = " thisClass = new ";
+  protected final String TEXT_14 = "();" + NL + "\t\tthisClass.createSShell() ;" + NL + "\t\tthisClass.sShell.open();" + NL + "\t\t" + NL + "\t\twhile (!thisClass.sShell.isDisposed()) {" + NL + "\t\t\tif (!display.readAndDispatch()) display.sleep ();" + NL + "\t\t}" + NL + "\t\tdisplay.dispose();\t\t" + NL + "\t}";
+  protected final String TEXT_15 = NL;
 
 public String generateSource(String typeName, String superClassName, HashMap argumentMatrix)
   {
@@ -45,26 +51,43 @@ public String generateSource(String typeName, String superClassName, HashMap arg
 
     
 	boolean createMain = (argumentMatrix != null && ((String)argumentMatrix.get(CREATE_MAIN)).equals("true"));
+	String layout = org.eclipse.ve.internal.swt.SwtPlugin.getDefault().getPluginPreferences().getString(org.eclipse.ve.internal.swt.SwtPlugin.DEFAULT_LAYOUT);
+	boolean isNullLayout = layout.equals(org.eclipse.ve.internal.swt.SwtPlugin.NULL_LAYOUT);
+	String layoutName = "";
+	if (!isNullLayout)
+		layoutName = layout.substring(layout.lastIndexOf('.')+1, layout.length());
 	 
     stringBuffer.append(TEXT_1);
     if(createMain){
     stringBuffer.append(TEXT_2);
     }
     stringBuffer.append(TEXT_3);
-    stringBuffer.append(typeName);
+    if(!isNullLayout){
     stringBuffer.append(TEXT_4);
-    stringBuffer.append(org.eclipse.swt.widgets.Shell.class.getName().substring(org.eclipse.swt.widgets.Shell.class.getName().lastIndexOf(".")+1));
+    stringBuffer.append(layout);
     stringBuffer.append(TEXT_5);
-     	
-	if (createMain) {
+    }
     stringBuffer.append(TEXT_6);
     stringBuffer.append(typeName);
     stringBuffer.append(TEXT_7);
-    stringBuffer.append(typeName);
+    stringBuffer.append(org.eclipse.swt.widgets.Shell.class.getName().substring(org.eclipse.swt.widgets.Shell.class.getName().lastIndexOf(".")+1));
     stringBuffer.append(TEXT_8);
+    if(!isNullLayout){
+    stringBuffer.append(TEXT_9);
+    stringBuffer.append(layoutName);
+    stringBuffer.append(TEXT_10);
+    }
+    stringBuffer.append(TEXT_11);
+     	
+	if (createMain) {
+    stringBuffer.append(TEXT_12);
+    stringBuffer.append(typeName);
+    stringBuffer.append(TEXT_13);
+    stringBuffer.append(typeName);
+    stringBuffer.append(TEXT_14);
     
 	}
-    stringBuffer.append(TEXT_9);
+    stringBuffer.append(TEXT_15);
     return stringBuffer.toString();
   }
 }
