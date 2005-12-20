1d757480d956ff650dcf58ba1385e0cae9ec5e45
gmendel <gmendel@f56db7ec-86c8-4192-a518-b33116ffafde>
2005-12-20 17:14:26 +0000

    Phoenix support

--- website/WebContent/_projectCommon.php
+++ website/WebContent/_projectCommon.php
@@ -9,9 +9,11 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
-	$Nav->addNavSeparator("Project Home", 	"downloads.php");
-	$Nav->addCustomNav("Downloads", 		"http://download.eclipse.org/tools/ve/downloads/index.php", 	"_self", 2);
+	$Nav->addNavSeparator("VE Home", 	"http://eclipse.org/vep");
+	$Nav->addCustomNav("VE Downloads", 		"http://download.eclipse.org/tools/ve/downloads/index.php", 	"_self", 1);
 	$Nav->addCustomNav("Installation", 		"install.php", 		"_self", 2);
-	$Nav->addCustomNav("FAQ", 				"faq.html", 			"_self", 2);
+	$Nav->addCustomNav("VE FAQ", 				"faq.html", 			"_self", 3);
+	$Nav->addCustomNav("Download Archives ", 		"http://archive.eclipse.org/tools/ve/downloads/", 	"_self", 2);
+
 
 ?>
