4d92543bbe0e494394014b31458aa43d65f03b37
nickb <nickb@f56db7ec-86c8-4192-a518-b33116ffafde>
2009-06-09 21:35:47 +0000

    new VE homepage, nav, updates, downloads

---
+++ trunk/website/downloads/index.php
@@ -0,0 +1,70 @@
+<?php
+$buildServerCommonFiles = array( 
+	"/opt/public/cbi/modeling/includes/buildServer-common.php",
+	$_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/buildServer-common.php",
+);
+$foundBuildServerCommonFile = false;
+foreach ($buildServerCommonFiles as $bs)
+{
+	if (is_file($bs))
+	{
+		require_once($bs); $foundBuildServerCommonFile = true; break;
+	}
+}
+if (!$foundBuildServerCommonFile)
+{
+	print "Warning: could not find modeling/includes/buildServer-common.php in _common.php";
+}
+else
+{
+	unset($foundBuildServerCommonFile);
+}
+
+$defaultProj = "";
+unset($_GET["project"]); // pdt has no subprojects, so this should be blank
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");  require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); $App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon());
+ob_start();
+
+/* config */
+
+/* zips that are allowed to be absent from the downloads page (eg., new ones added mid-stream) */
+$extraZips = array();
+
+/* $project => sections/Project Name => (prettyname => filename) */
+/* only required if using something other than the default 4; otherwise will be generated */
+$dls = array(
+	"/" => array( # use "/" because PDT has no parent or child projects/components
+		"PDT" => array(
+		 	"<acronym title=\"Click to download archived All-In-One p2 Repo Update Site\"><img alt=\"Click to download archived All-In-One p2 Repo Update Site\" src=\"/modeling/images/dl-icon-update-zip.gif\"/> <b style=\"color:green\">All-In-One Update Site</b></acronym>" => "Update",
+			"Automated Tests" => "Automated-Tests",
+		),
+	),
+);
+
+/* list of valid file prefixes for projects who have been renamed; keys have leading / to match $proj */
+/* only required if using something other than the default; otherwise will be generated */
+$filePre = array( # use "/" because PDT has no parent or child projects/components
+	"/" => array("VE") // PDT-sdk-*.zip
+);
+
+/* define showNotes(), $oldrels, doLanguagePacks() in extras-$proj.php (or just extras.php for flat projects) if necessary, downloads-common.php will include them */
+/* end config */
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/downloads-common.php");
+
+$html = ob_get_contents();
+ob_end_clean();
+
+/* Note: Google Analytics moved to _projectCommon.php so it's on EVERY page */
+
+$trans = array_flip($projects);
+$pageTitle = "Visual Editor (VE) - Downloads";
+$pageKeywords = ""; // TODO: add something here
+$pageAuthor = "Nick Boldt";
+
+# Generate the web page
+$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/modeling/includes/downloads.css"/>' . "\n");
+$App->AddExtraHtmlHeader('<script src="/modeling/includes/downloads.js" type="text/javascript"></script>' . "\n"); //ie doesn't understand self closing script tags, and won't even try to render the page if you use one
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+
+?>
---
+++ trunk/website/index.php
@@ -0,0 +1,132 @@
+<?php  																														
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+$App = new App(); $Nav = new Nav(); $Menu = new Menu(); include($App->getProjectCommon()); 	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "Visual Editor Project";
+	$pageKeywords	= "visual, editor, java, eclipse, graphical, ui, swing, swt, jfc, rcp";
+	$pageAuthor		= "Nick Boldt";
+	
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+	<div id="midcolumn">
+
+	    <table border="0" cellpadding="5">
+	      <tr>
+	        <td valign="top">
+
+	        <h1>$pageTitle</h1>
+		<h3>First-class GUI building tools for Eclipse</h3>
+
+		<p>&nbsp;</p>
+		<div class="homeitem3col">
+			<p>The Eclipse Visual Editor project is a vendor-neutral, open development platform supplying frameworks for 
+			creating GUI builders, and exemplary, extensible tool implementations for Swing/JFC and SWT/RCP. 
+			These tools are exemplary in that they verify the utility of the Eclipse Visual Editor frameworks, 
+			illustrate the appropriate use of those frameworks, and support the development and maintenance of the 
+			Eclipse Visual Editor Platform itself.</p> 
+			
+			<p>The purpose of the Eclipse Visual Editor Project is to advance the creation, evolution, promotion of the Eclipse Visual Editor platform, 
+			and to cultivate both an open source community and an ecosystem of complementary products, capabilities, and services. In particular, the 
+			Visual Editor Project intends to be useful for creating GUI builders for other languages such as C/C++ and alternate widget sets, 
+			including those that are not supported under Java. </p>
+
+	          <table border="0" cellpadding="5">
+
+	            <!-- Downloads -->
+	            <tr>
+	              <td width=20></td>
+	              <td><a href="downloads/"><img border=0 src="images/downloads.gif"></a></td>
+	              <td>
+	                <table border="0" cellpadding="0">
+	                  <tr>
+	                    <td><font size=+1><a href="downloads/">Downloads</a></font></td>
+	                  </tr>
+	                  <tr>
+	                    <td>Get our latest <b>1.4.0</b> nightly build!</td>
+	                  </tr>
+	                </table>
+	              </td>
+	            </tr>
+
+	            <!-- Get Involved -->
+	            <tr>
+	              <td width=20></td>
+	              <td><a href="http://wiki.eclipse.org/VE"><img border=0 src="images/community.jpg"></a></td>
+	              <td>
+	                <table border="0" cellpadding="0">
+	                  <tr>
+	                    <td><font size=+1><a href="http://wiki.eclipse.org/VE">Get Involved</a></font></td>
+	                  </tr>
+	                  <tr>
+	                    <td>Find out how you can get involved with the project</td>
+	                  </tr>
+	                </table>
+	              </td>
+	            </tr>
+
+	            <!-- Plans - should be http://www.eclipse.org/projects/project-plan.php?projectid=tools.ve -->
+	            <tr>
+	              <td width=20></td>
+	              <td><a href="http://wiki.eclipse.org/VE"><img border=0 src="images/reference.png"></a></td>
+	              <td>
+	                <table border="0" cellpadding="0">
+	                  <tr>
+	                    <td><font size=+1><a href="http://wiki.eclipse.org/VE">Plans</a></font></td>
+	                  </tr>
+	                  <tr>
+	                    <td>See the project plans</td>
+	                  </tr>
+	                </table>
+	              </td>
+	            </tr>
+
+	          </table>
+	        </td>
+	      </tr>
+	    </table>
+	</div>
+           
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>VEP Components</h6>
+			<ul>
+				<li><a href="docs/components/cde/cde.html"> CDE </a></li>
+				<li><a href="docs/components/core/core.html"> Core </a></li>
+				<li><a href="docs/components/jfc/jfc.html"> JFC/Swing</a> </li>
+				<li><a href="docs/components/javamodel/javaModel.html"> Java Model</a></li>
+				<li><a href="docs/components/swt/swt.html">SWT</a></li>
+			</ul>
+		</div>
+
+		<div class="sideitem">
+			<h6>Related links</h6>
+			<ul>
+				<li><a href="http://www.eclipse.org/galileo">Eclipse 3.5 Galileo Release</a></li>
+				<li><a href="http://www.eclipse.org/gef/">Graphical Editing Framework (GEF)</a></li>
+				<li><a href="http://www.eclipse.org/modeling/emf/">Eclipse Modeling Framework (EMF)</a> </li>
+				<li><a href="http://wiki.eclipse.org/index.php/JFace_Data_Binding ">JFace Data Binding</a></li>				
+			</ul>
+		</div>
+	</div>
+	
+	<!--
+	<div id="rightcolumn">
+		<div class="sideitem">
+			<h6>News</h6>
+			<ul>
+				<li><a href="?">news</a></li> 
+			</ul>
+		</div>
+	</div>
+	-->
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
