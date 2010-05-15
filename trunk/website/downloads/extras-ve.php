7e7237b2ea5c682fa16fab4297082990e991c09a
yvyang <yvyang@f56db7ec-86c8-4192-a518-b33116ffafde>
2010-05-15 12:48:52 +0000

    extras-vep should be extras-ve through downloads-common.php

---
+++ trunk/website/downloads/extras-ve.php
@@ -0,0 +1,26 @@
+<?php
+
+$oldrels = array(
+	"1.2.3" => array("2007/01/30 11:17", "http://archive.eclipse.org/tools/ve/downloads/drops/R-1.2.3_jem-200701301117/"),
+);
+
+function doRequirements()
+{ }
+
+function showNotes()
+{
+	global $PWD;
+?>
+
+	<div class="homeitem3col">
+		<h3>Questions?</h3>
+		<p>If you have problems downloading the drops or with this site, contact the <a href="mailto:webmaster@eclipse.org">webmaster</a>.</p>
+		<p>Looking for the <a href="http://archive.eclipse.org/tools/ve/downloads/">old VE download page</a>? You can find it <a href="http://archive.eclipse.org/tools/ve/downloads/">here</a>.</p>
+		<p>Looking for the <a href="http://eclipse.org/vep/WebContent/docs/translations/translation.html">old VE translations page</a>? You can find it <a href="http://eclipse.org/vep/WebContent/docs/translations/translation.html">here</a>.</p>
+		
+		<p>All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.html">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
+	</div>
+<?php
+}
+
+?>
